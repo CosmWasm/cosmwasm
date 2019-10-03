98c9b048d3e11353a05bd9d1ea85c3f929d0dd11
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-03 10:09:07 +0200

    Clean up conditional compilation with embedded submodules

--- contracts/hackatom/src/imports.rs
+++ contracts/hackatom/src/imports.rs
@@ -6,61 +6,69 @@ pub trait Storage {
 }
 
 #[cfg(target_arch = "wasm32")]
-use std::os::raw::{c_char};
+pub use wasm::{ExternalStorage};
+
+#[cfg(test)]
+pub use mock::{MockStorage};
 
 #[cfg(target_arch = "wasm32")]
-extern "C" {
-    fn c_read() -> *mut c_char;
-    fn c_write(string: *mut c_char);
-}
+mod wasm {
+    extern "C" {
+        fn c_read() -> *mut c_char;
+        fn c_write(string: *mut c_char);
+    }
 
-#[cfg(target_arch = "wasm32")]
-pub struct ExternalStorage {}
+    use super::*;
+    use std::os::raw::{c_char};
 
-#[cfg(target_arch = "wasm32")]
-impl Storage for ExternalStorage {
-    fn get_state(&self) -> Option<Vec<u8>> {
-        use std::ffi::{CStr};
-        unsafe {
-            let ptr = c_read();
-            if ptr.is_null() {
-                return None;
+    pub struct ExternalStorage {}
+
+    impl Storage for ExternalStorage {
+        fn get_state(&self) -> Option<Vec<u8>> {
+            use std::ffi::{CStr};
+            unsafe {
+                let ptr = c_read();
+                if ptr.is_null() {
+                    return None;
+                }
+                let state = CStr::from_ptr(ptr).to_bytes().to_vec();
+                return Some(state);
             }
-            let state = CStr::from_ptr(ptr).to_bytes().to_vec();
-            return Some(state);
         }
-    }
 
-    fn set_state(&mut self, state: Vec<u8>) {
-        use std::ffi::{CString};
-        unsafe {
-            c_write(CString::new(state).unwrap().into_raw());
+        fn set_state(&mut self, state: Vec<u8>) {
+            use std::ffi::{CString};
+            unsafe {
+                c_write(CString::new(state).unwrap().into_raw());
+            }
         }
     }
 }
 
 #[cfg(test)]
-pub struct MockStorage {
-    data: Option<Vec<u8>>
-}
+mod mock {
+    use super::*;
 
-#[cfg(test)]
-impl MockStorage {
-    pub fn new() -> MockStorage {
-        MockStorage{data: None}
+    pub struct MockStorage {
+        data: Option<Vec<u8>>
     }
-}
 
-#[cfg(test)]
-impl Storage for &mut MockStorage {
-    fn get_state(&self) -> Option<Vec<u8>> {
-        match &self.data {
-            Some(v) => Some(v.clone()),
-            None => None,
+    impl MockStorage {
+        pub fn new() -> MockStorage {
+            MockStorage{data: None}
         }
     }
 
-    fn set_state(&mut self, state: Vec<u8>) {
-        self.data = Some(state);
+    impl Storage for &mut MockStorage {
+        fn get_state(&self) -> Option<Vec<u8>> {
+            match &self.data {
+                Some(v) => Some(v.clone()),
+                None => None,
+            }
+        }
+
+        fn set_state(&mut self, state: Vec<u8>) {
+            self.data = Some(state);
+        }
     }
 }
--- contracts/hackatom/src/lib.rs
+++ contracts/hackatom/src/lib.rs
@@ -1,25 +1,28 @@
 pub mod contract;
-pub mod imports;
 pub mod types;
 
-/** Below we expose wasm exports **/
+mod imports;
 
+/** Below we expose wasm exports **/
 #[cfg(target_arch = "wasm32")]
 mod exports;
 
 #[cfg(target_arch = "wasm32")]
-pub use crate::exports::{allocate, deallocate};
-#[cfg(target_arch = "wasm32")]
-use std::os::raw::{c_char};
+mod wasm {
+    use super::*;
+    use std::os::raw::{c_char};
+    pub use exports::{allocate, deallocate};
 
-#[cfg(target_arch = "wasm32")]
-#[no_mangle]
-pub extern "C" fn init_wrapper(params_ptr: *mut c_char, msg_ptr: *mut c_char) -> *mut c_char {
-    exports::init(&contract::init::<imports::ExternalStorage>, params_ptr, msg_ptr)
+    #[no_mangle]
+    pub extern "C" fn init_wrapper(params_ptr: *mut c_char, msg_ptr: *mut c_char) -> *mut c_char {
+        exports::init(&contract::init::<imports::ExternalStorage>, params_ptr, msg_ptr)
+    }
+
+    #[no_mangle]
+    pub extern "C" fn send_wrapper(params_ptr: *mut c_char, msg_ptr: *mut c_char) -> *mut c_char {
+        exports::send(&contract::send::<imports::ExternalStorage>, params_ptr, msg_ptr)
+    }
 }
 
 #[cfg(target_arch = "wasm32")]
-#[no_mangle]
-pub extern "C" fn send_wrapper(params_ptr: *mut c_char, msg_ptr: *mut c_char) -> *mut c_char {
-    exports::send(&contract::send::<imports::ExternalStorage>, params_ptr, msg_ptr)
-}
+pub use wasm::{allocate, deallocate, init_wrapper, send_wrapper};
