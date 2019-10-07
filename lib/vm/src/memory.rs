db30cd45f581d5faa0d778ff768f2e4826f5c7e7
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-07 17:25:18 +0200

    Start using WasmPtr, convert ptrs to u32

--- lib/vm/src/memory.rs
+++ lib/vm/src/memory.rs
@@ -1,4 +1,5 @@
 use wasmer_runtime::{Ctx, Func, Instance};
+use wasmer_runtime_core::memory::ptr::WasmPtr;
 
 use cosmwasm::memory::Slice;
 
@@ -7,15 +8,15 @@ use cosmwasm::memory::Slice;
 // write_mem allocates memory in the instance and copies the given data in
 // returns the memory offset, to be passed as an argument
 // panics on any error (TODO, use result?)
-pub fn allocate(instance: &mut Instance, data: &[u8]) -> i32 {
+pub fn allocate(instance: &mut Instance, data: &[u8]) -> u32 {
     // allocate
-    let alloc: Func<(i32), (i32)> = instance.func("allocate").unwrap();
-    let ptr = alloc.call(data.len() as i32).unwrap();
+    let alloc: Func<(u32), (u32)> = instance.func("allocate").unwrap();
+    let ptr = alloc.call(data.len() as u32).unwrap();
     write_memory(instance.context(), ptr, data);
     ptr
 }
 
-pub fn read_memory(ctx: &Ctx, ptr: i32) -> Vec<u8> {
+pub fn read_memory(ctx: &Ctx, ptr: u32) -> Vec<u8> {
     let slice = to_slice(ctx, ptr);
     let (start, end) = (slice.offset, slice.offset + slice.len);
     let memory = &ctx.memory(0).view::<u8>()[start..end];
@@ -30,7 +31,7 @@ pub fn read_memory(ctx: &Ctx, ptr: i32) -> Vec<u8> {
 
 // write_memory returns how many bytes written on success
 // negative result is how many bytes requested if too small
-pub fn write_memory(ctx: &Ctx, ptr: i32, data: &[u8]) -> i32 {
+pub fn write_memory(ctx: &Ctx, ptr: u32, data: &[u8]) -> i32 {
     let slice = to_slice(ctx, ptr);
     if data.len() > slice.len {
         return -(data.len() as i32);
@@ -49,11 +50,14 @@ pub fn write_memory(ctx: &Ctx, ptr: i32, data: &[u8]) -> i32 {
 }
 
 // to_slice reads in a ptr to slice in wasm memory and constructs the object we can use to access it
-fn to_slice(ctx: &Ctx, ptr: i32) -> Slice {
-    let buf_ptr = (ptr / 4) as usize; // convert from u8 to i32 offset
-    let memory = &ctx.memory(0).view::<i32>();
+fn to_slice(ctx: &Ctx, ptr: u32) -> Slice {
+    let memory = &ctx.memory(0);
+    let offset_ptr = WasmPtr::<i32>::new(ptr);
+    let len_ptr = WasmPtr::<i32>::new(ptr+4);
+    let offset = offset_ptr.deref(memory).map_or(0, |x| x.get());
+    let len = len_ptr.deref(memory).map_or(0, |x| x.get());
     Slice {
-        offset: memory[buf_ptr].get() as usize,
-        len: memory[buf_ptr + 1].get() as usize,
+        offset: offset as usize,
+        len: len as usize,
     }
 }
