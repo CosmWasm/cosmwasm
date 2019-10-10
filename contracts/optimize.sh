5c7e5ecf96e53642cf162d990b99d58058bc19a7
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-10 18:21:19 +0200

    Only use absolute imports, Dockerfile compiles

--- contracts/optimize.sh
+++ contracts/optimize.sh
@@ -3,18 +3,12 @@
 set -e
 
 export PATH=$PATH:/root/.cargo/bin
-echo $PATH
-which wasm-pack
-which wasm-opt
-
 outdir=$(mktemp -d)
-echo
-pwd
-ls
-echo wasm-pack build --out-dir "${outdir}"
 
+echo wasm-pack build --out-dir "${outdir}"
 wasm-pack build --out-dir "${outdir}" || true
-wasm-pack build --out-dir "${outdir}" || true
+
 echo wasm-opt -Os "${outdir}"/*.wasm -o contract.wasm
 wasm-opt -Os "${outdir}"/*.wasm -o contract.wasm
+
 echo "done"
