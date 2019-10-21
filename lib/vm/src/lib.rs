6009d2c249d58c6f58bc5f575939e785689ea341
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-21 16:35:17 +0200

    Expose init/handle calls

--- lib/vm/src/lib.rs
+++ lib/vm/src/lib.rs
@@ -6,6 +6,6 @@ mod wasm_store;
 mod wasmer;
 
 pub use crate::cache::Cache;
-pub use crate::calls::{call_handle, call_init};
+pub use crate::calls::{call_handle, call_handle_raw, call_init, call_init_raw};
 pub use crate::memory::{allocate, read_memory};
 pub use crate::wasmer::{instantiate, with_storage};
