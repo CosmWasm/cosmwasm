b2ec49506fa5710e451e5feb3ce2668a7a627e6b
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-21 16:08:33 +0200

    Start building cache

---
+++ lib/vm/src/cache.rs
@@ -0,0 +1,44 @@
+use std::fs::{DirBuilder, File, OpenOptions};
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
+
+use failure::Error;
+
+use crate::wasm_store::{ensure_dir, load, save};
+use crate::wasmer::{Instance, instantiate};
+
+pub struct Cache {
+    wasm_dir: PathBuf,
+}
+
+static WASM_DIR: &str = "wasm";
+
+impl Cache {
+    /// new stores the data for cache under base_dir
+    pub fn new(base_dir: &str) -> Self {
+        let  wasm_dir = Path::new(base_dir).join(WASM_DIR);
+        ensure_dir(wasm_dir.to_str().unwrap());
+        Cache{ wasm_dir }
+    }
+
+    fn wasm_path(&self) -> &str {
+        self.wasm_dir.to_str().unwrap()
+    }
+}
+
+impl Cache {
+    pub fn save_wasm(&self, wasm: &[u8]) -> Result<Vec<u8>, Error> {
+        save(self.wasm_path(), wasm)
+    }
+
+    pub fn load_wasm(&self, id: &[u8]) -> Result<Vec<u8>, Error> {
+        load(self.wasm_path(), id)
+    }
+
+    /// get instance returns a wasmer Instance tied to a previously saved wasm
+    pub fn get_instance(&self, id: &[u8]) -> Result<Instance, Error> {
+        // TODO: we can definitely add some caches (module on disk, instance in memory) to make this faster
+        let wasm = self.load_wasm(id)?;
+        Ok(instantiate(&wasm))
+    }
+}
--- lib/vm/src/lib.rs
+++ lib/vm/src/lib.rs
@@ -1,3 +1,4 @@
+mod cache;
 mod calls;
 mod exports;
 mod memory;
