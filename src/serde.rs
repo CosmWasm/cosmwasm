0e4d4214a7b6b54fdcced19647b590f1bbec8360
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-26 01:17:21 +0200

    Use serde-json-wasm in cosmwasm

--- src/serde.rs
+++ src/serde.rs
@@ -2,4 +2,4 @@
 // The reason is two fold:
 // 1. To easily ensure that all calling libraries use the same version (minimize code size)
 // 2. To allow us to switch out to eg. serde-json-core more easily
-pub use serde_json::{from_slice, to_vec};
+pub use serde_json_wasm::{from_slice, to_vec};
