57494e8097bb2649dd6e2abaf9fa66312ea60b8d
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-27 11:19:03 +0100

    Fix typo in cranelift mock

--- lib/vm/src/backends/cranelift.rs
+++ lib/vm/src/backends/cranelift.rs
@@ -2,7 +2,7 @@
 use wasmer_clif_backend::CraneliftCompiler;
 use wasmer_runtime::{compile_with, Backend, Instance, Module};
 
-static FAKE_GAS_AVAILBLE: u64 = 1_000_000;
+static FAKE_GAS_AVAILABLE: u64 = 1_000_000;
 
 pub fn compile(code: &[u8]) -> Module {
     compile_with(code, &CraneliftCompiler::new()).unwrap()
