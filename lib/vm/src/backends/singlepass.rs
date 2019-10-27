4e074f6f1231e273fd6100adde581c2ada6d772e
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-27 10:42:40 +0100

    Use metered compiler for singlepass

--- lib/vm/src/backends/singlepass.rs
+++ lib/vm/src/backends/singlepass.rs
@@ -1,9 +1,18 @@
 #![cfg(feature = "singlepass")]
-use wasmer_singlepass_backend::SinglePassCompiler;
+use wasmer_middleware_common::metering;
 use wasmer_runtime::{compile_with, Backend, Module};
+use wasmer_runtime_core::codegen::{MiddlewareChain, StreamingCompiler};
+use wasmer_singlepass_backend::ModuleCodeGenerator as SinglePassMCG;
+
+static GAS_LIMIT: u64 = 10_000_000_000;
 
 pub fn compile(code: &[u8]) -> Module {
-    compile_with(code, &SinglePassCompiler::new()).unwrap()
+    let c: StreamingCompiler<SinglePassMCG, _, _, _, _> = StreamingCompiler::new(move || {
+        let mut chain = MiddlewareChain::new();
+        chain.push(metering::Metering::new(GAS_LIMIT));
+        chain
+    });
+    compile_with(code, &c).unwrap()
 }
 
 pub fn backend() -> Backend {
