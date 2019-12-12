d013ecec6bb69767c792c73f0f28aff896a304b2
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-12 22:39:14 +0100

    Add Addresser trait and MockAddresser

--- src/mock.rs
+++ src/mock.rs
@@ -1,6 +1,10 @@
 use std::collections::HashMap;
+use std::str::from_utf8;
 
-pub use crate::storage::Storage;
+use snafu::ResultExt;
+
+use crate::storage::{Addresser, Storage};
+use crate::errors::{ContractErr, Result, Utf8Err};
 
 #[derive(Clone)]
 pub struct MockStorage {
@@ -31,6 +35,49 @@ impl Storage for MockStorage {
     }
 }
 
+// MockAddresser zero pads all human addresses to make them fit the canonical_length
+// it trims off zeros for the reverse operation.
+// not really smart, but allows us to see a difference (and consistent length for canonical adddresses)
+#[derive(Clone)]
+pub struct MockAddresser {
+    canonical_length: usize,
+}
+
+impl MockAddresser {
+    pub fn new(canonical_length: usize) -> Self {
+        MockAddresser { canonical_length }
+    }
+}
+
+impl Default for MockAddresser {
+    fn default() -> Self {
+        Self::new(20)
+    }
+}
+
+impl Addresser for MockAddresser {
+    fn canonicalize(&self, human: &str) -> Result<Vec<u8>> {
+        if human.len() > self.canonical_length {
+            return ContractErr { msg: "human encoding too long" }.fail();
+        }
+        let mut out = human.as_bytes().to_vec();
+        let append = self.canonical_length - out.len();
+        if append > 0 {
+            out.extend(vec![0u8; append]);
+        }
+        Ok(out)
+    }
+
+    fn humanize(&self, canonical: &[u8]) -> Result<String> {
+        // remove trailing 0's (TODO: fix this - but fine for first tests)
+        let trimmed: Vec<u8> = canonical.iter().cloned().filter(|&x| x != 0).collect();
+        // convert to utf8
+        let human = from_utf8(&trimmed).context(Utf8Err{})?;
+        Ok(human.to_string())
+    }
+}
+
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -42,5 +89,26 @@ mod test {
         store.set(b"foo", b"bar");
         assert_eq!(Some(b"bar".to_vec()), store.get(b"foo"));
         assert_eq!(None, store.get(b"food"));
+    }
+
+    #[test]
+    fn flip_addresses() {
+        let addr = MockAddresser::new(20);
+        let human = "shorty";
+        let canon = addr.canonicalize(&human).unwrap();
+        assert_eq!(canon.len(), 20);
+        assert_eq!(&canon[0..6], human.as_bytes());
+        assert_eq!(&canon[6..], &[0u8; 14]);
+
+        let recovered = addr.humanize(&canon).unwrap();
+        assert_eq!(human, &recovered);
+    }
+
+    #[test]
+    #[should_panic]
+    fn canonical_length_enforced() {
+        let addr = MockAddresser::new(10);
+        let human = "longer-than-10";
+        let _ = addr.canonicalize(&human).unwrap();
     }
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,6 +1,15 @@
 use std::vec::Vec;
 
+use crate::errors::Result;
+
+// Storage is access to the contracts persistent data store
 pub trait Storage {
     fn get(&self, key: &[u8]) -> Option<Vec<u8>>;
     fn set(&mut self, key: &[u8], value: &[u8]);
+}
+
+// Addresser provides platform-specific callbacks for converting addresses
+pub trait Addresser {
+    fn canonicalize(&self, human: &str) -> Result<Vec<u8>>;
+    fn humanize(&self, canonical: &[u8]) -> Result<String>;
 }
