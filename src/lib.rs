1f19fab313e2e06282a61d4b3cece5835d326394
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-12 15:03:37 +0100

    Hit rust language limits

--- src/lib.rs
+++ src/lib.rs
@@ -36,3 +36,34 @@ fn do_call(data: &[u8]) -> crate::errors::Result<Vec<u8>> {
     // TODO: this makes some external function call
     Ok(b"{}".to_vec())
 }
+
+/** try using this... see what the compiler says **/
+
+// Note: this doesn't work as written.
+// Maybe we would need to try an approach like https://docs.rs/refl/0.2.0/refl/
+
+pub enum MyRequest {
+    A(i32, i32),
+    B(String),
+}
+
+pub enum MyResponse {
+    A(i32),
+    B(Vec<u8>),
+}
+
+// This won't compile, cannot impl on enum varinats
+// Relevant (open) RFC is at https://github.com/rust-lang/rfcs/pull/2593
+impl Request for MyRequest::A {
+    type Response = MyResponse::A;
+}
+
+impl Request for MyRequest::B {
+    type Response = MyResponse::B;
+}
+
+fn demo() {
+    let a = MyRequest::A(7, 12);
+    let b = call(a).unwrap();
+    assert_eq!(b.0, 19);
+}
