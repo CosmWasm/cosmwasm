31fb248ac1a655fcb4d9c6b772aa0f13b1f3466e
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-12 14:43:05 +0100

    Define trait

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,30 @@ pub mod query;
 pub mod serde;
 pub mod storage;
 pub mod types;
+
+
+/** demo **/
+
+use snafu::ResultExt;
+
+pub trait Request: ::serde::ser::Serialize {
+    type Response: ::serde::de::DeserializeOwned;
+
+    fn ser(&self) -> crate::errors::Result<Vec<u8>> {
+        serde::to_vec(&self).context(crate::errors::SerializeErr{kind: "Request"})
+    }
+
+    fn de(output: &[u8]) -> crate::errors::Result<Self::Response> {
+        serde::from_slice(output).context(crate::errors::ParseErr{kind: "Request"})
+    }
+}
+
+pub fn call<T: Request>(data: T) -> crate::errors::Result<T::Response> {
+    let output = do_call(&data.ser()?)?;
+    T::de(&output)
+}
+
+fn do_call(data: &[u8]) -> crate::errors::Result<Vec<u8>> {
+    // TODO: this makes some external function call
+    Ok(b"{}".to_vec())
+}
