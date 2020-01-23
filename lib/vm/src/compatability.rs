2c2582794fee92c46cfb3deefa4d6f24ad4fe141
Simon Warta <simon@warta.it>
2020-01-23 19:02:15 +0100

    Clarify how import_requirements_satisfied works

--- lib/vm/src/compatability.rs
+++ lib/vm/src/compatability.rs
@@ -37,7 +37,7 @@ static MISSING_EXPORT_MSG: &str = "WASM doesn't have required exports - version 
 pub fn check_api_compatibility(wasm_code: &[u8]) -> Result<()> {
     let mut reader = std::io::Cursor::new(wasm_code);
     let symbols = wasm_nm::symbols(PUBLIC_SYMBOLS.clone(), &mut reader).unwrap();
-    if !only_imports(&symbols, SUPPORTED_IMPORTS) {
+    if !import_requirements_satisfied(&symbols, SUPPORTED_IMPORTS) {
         return ValidationErr {
             msg: EXTRA_IMPORT_MSG,
         }
@@ -52,8 +52,11 @@ pub fn check_api_compatibility(wasm_code: &[u8]) -> Result<()> {
     Ok(())
 }
 
-fn only_imports(symbols: &Symbols, allowed: &[&str]) -> bool {
-    let imports: Vec<&str> = symbols
+/// Checks if the import requirements of the contract are satisfied.
+/// When this is not the case, we either have an incompatibility between contract and VM
+/// or a error in the contract.
+fn import_requirements_satisfied(symbols: &Symbols, supported_imports: &[&str]) -> bool {
+    let required_imports: Vec<&str> = symbols
         .iter()
         .filter_map(|s| match s {
             Symbol::Import { name } => Some(name),
@@ -61,8 +64,8 @@ fn only_imports(symbols: &Symbols, allowed: &[&str]) -> bool {
         })
         .collect();
 
-    for i in imports {
-        if !allowed.contains(&i) {
+    for required_import in required_imports {
+        if !supported_imports.contains(&required_import) {
             return false;
         }
     }
@@ -99,11 +102,11 @@ mod test {
         let symbols = wasm_nm::symbols(PUBLIC_SYMBOLS.clone(), &mut reader).unwrap();
 
         // if contract has more than we provide, bad
-        let imports_good = only_imports(&symbols, &["c_read", "c_write"]);
+        let imports_good = import_requirements_satisfied(&symbols, &["c_read", "c_write"]);
         assert_eq!(imports_good, false);
 
         // exact match good
-        let imports_good = only_imports(
+        let imports_good = import_requirements_satisfied(
             &symbols,
             &[
                 "c_read",
@@ -115,7 +118,7 @@ mod test {
         assert_eq!(imports_good, true);
 
         // if we provide more, also good
-        let imports_good = only_imports(
+        let imports_good = import_requirements_satisfied(
             &symbols,
             &[
                 "c_read",
