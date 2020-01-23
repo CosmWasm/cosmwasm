762985e33b03bf44b20c5115f03f43da8c9649ce
Simon Warta <simon@warta.it>
2020-01-23 20:55:23 +0100

    Update gas cost for singlepass tests

--- lib/vm/src/instance.rs
+++ lib/vm/src/instance.rs
@@ -186,7 +186,7 @@ mod test {
 
         let init_used = orig_gas - instance.get_gas();
         println!("init used: {}", init_used);
-        assert_eq!(init_used, 66_763);
+        assert_eq!(init_used, 66_762);
 
         // run contract - just sanity check - results validate in contract unit tests
         instance.set_gas(orig_gas);
@@ -203,7 +203,7 @@ mod test {
 
         let handle_used = orig_gas - instance.get_gas();
         println!("handle used: {}", handle_used);
-        assert_eq!(handle_used, 110_267);
+        assert_eq!(handle_used, 110_131);
     }
 
     #[test]
@@ -242,6 +242,6 @@ mod test {
 
         let query_used = orig_gas - instance.get_gas();
         println!("query used: {}", query_used);
-        assert_eq!(query_used, 58_033);
+        assert_eq!(query_used, 58_038);
     }
 }
