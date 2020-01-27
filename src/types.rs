9ad2a3eb90e1525709cf4e56a7f0e96d062b0f3c
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-01-27 15:29:00 +0100

    Add Base64 type

--- src/types.rs
+++ src/types.rs
@@ -2,6 +2,54 @@ use std::fmt;
 
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use snafu::ResultExt;
+
+use crate::errors::{Base64Err, Result};
+
+#[derive(Serialize, Deserialize, Clone, Default, Debug, PartialEq, JsonSchema)]
+pub struct Base64(pub String);
+
+impl From<&str> for Base64 {
+    fn from(data: &str) -> Self {
+        Base64(data.to_string())
+    }
+}
+
+impl From<String> for Base64 {
+    fn from(data: String) -> Self {
+        Base64(data)
+    }
+}
+
+impl From<&[u8]> for Base64 {
+    fn from(data: &[u8]) -> Self {
+        Base64(base64::encode(data))
+    }
+}
+
+impl From<&Base64> for Base64 {
+    fn from(data: &Base64) -> Self {
+        Base64(data.0.to_string())
+    }
+}
+
+impl Base64 {
+    // as_bytes will return a &[u8] reference to the string format. This should be good
+    // for most apps (slightly longer, but saves the transform cost)
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+    // decode will return the underlying bytes after decoding base64
+    pub fn decode(&self) -> Result<Vec<u8>> {
+        base64::decode(&self.0).context(Base64Err {})
+    }
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
 
 #[derive(Serialize, Deserialize, Clone, Default, Debug, PartialEq, JsonSchema)]
 pub struct HumanAddr(pub String);
