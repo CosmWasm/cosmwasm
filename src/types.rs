c17ea3faf6c5417cbe98b415c7469d9b1768d708
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-02-25 20:50:40 +0100

    Change type of log return value in ContractResult

--- src/types.rs
+++ src/types.rs
@@ -141,11 +141,17 @@ impl ContractResult {
 }
 
 #[derive(Serialize, Deserialize, Clone, Default, Debug, PartialEq, JsonSchema)]
+pub struct LogAttribute {
+    pub key: String,
+    pub value: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Default, Debug, PartialEq, JsonSchema)]
 pub struct Response {
     // let's make the positive case a struct, it contrains Msg: {...}, but also Data, Log, maybe later Events, etc.
     pub messages: Vec<CosmosMsg>,
-    pub log: Option<String>,  // abci defines this as string
-    pub data: Option<Binary>, // abci defines this as bytes
+    pub log: Option<Vec<LogAttribute>>, // abci defines this as string
+    pub data: Option<Binary>,           // abci defines this as bytes
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -202,7 +208,10 @@ mod test {
                 to_address: HumanAddr("you".to_string()),
                 amount: coin("1015", "earth"),
             }],
-            log: Some("released funds!".to_string()),
+            log: Some(vec![LogAttribute {
+                key: "action".to_string(),
+                value: "release".to_string(),
+            }]),
             data: None,
         });
         let bin = to_vec(&send).expect("encode contract result");
