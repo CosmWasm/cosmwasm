2a75ee2ff9713cac783979f2cfdcb1cf7d4ffac3
Simon Warta <simon@warta.it>
2020-03-30 10:39:32 +0200

    Avoid unnecessary `use cosmwasm_std;`

--- contracts/queue/src/lib.rs
+++ contracts/queue/src/lib.rs
@@ -1,10 +1,6 @@
 pub mod contract;
 
 #[cfg(target_arch = "wasm32")]
-#[allow(unused_imports)]
-use cosmwasm_std; // Pull in cosmwasm_std's C externs (cosmwasm_vm_version_1, allocate, deallocate)
-
-#[cfg(target_arch = "wasm32")]
 mod wasm {
     use super::contract;
     use cosmwasm_std::{do_handle, do_init, do_query, ExternalApi, ExternalStorage};
@@ -32,4 +28,7 @@ mod wasm {
     extern "C" fn query(msg_ptr: *mut c_void) -> *mut c_void {
         do_query(&contract::query::<ExternalStorage, ExternalApi>, msg_ptr)
     }
+
+    // Other C externs like cosmwasm_vm_version_1, allocate, deallocate are available
+    // automatically because we `use cosmwasm_std`.
 }
