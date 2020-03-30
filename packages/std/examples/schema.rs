529090d2e015ec8d66edd4967ed93c6a85fc94b3
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-03-30 12:39:26 +0200

    QueryResult is ApiResult, but same JSON schema

--- packages/std/examples/schema.rs
+++ packages/std/examples/schema.rs
@@ -15,7 +15,7 @@ fn main() {
     export_schema(&schema_for!(CosmosMsg), &out_dir);
     export_schema(&schema_for!(InitResult), &out_dir);
     export_schema(&schema_for!(HandleResult), &out_dir);
-    export_schema(&schema_for!(QueryResult), &out_dir);
+    export_schema_with_title(&mut schema_for!(QueryResult), &out_dir, "QueryResult");
 }
 
 /// Writes schema to file. Overwrites existing file.
@@ -27,11 +27,28 @@ fn export_schema(schema: &RootSchema, out_dir: &PathBuf) -> () {
         .as_ref()
         .map(|b| b.title.clone().unwrap_or("untitled".to_string()))
         .unwrap_or("unknown".to_string());
+    write_schema(schema, out_dir, &title);
+}
+
+fn export_schema_with_title(schema: &mut RootSchema, out_dir: &PathBuf, title: &str) -> () {
+    // set the title explicitly on the schemas metadata
+    let metadata = &mut schema.schema.metadata;
+    if let Some(data) = metadata {
+        data.title = Some(title.to_string());
+    }
+    write_schema(schema, out_dir, &title);
+}
+
+
+fn write_schema(schema: &RootSchema, out_dir: &PathBuf, title: &str) -> () {
+    // first, we set the title as we wish
     let path = out_dir.join(format!("{}.json", to_snake_case(&title)));
     let json = serde_json::to_string_pretty(schema).unwrap();
     write(&path, json + "\n").unwrap();
     println!("Created {}", path.to_str().unwrap());
 }
+
+
 
 fn to_snake_case(name: &str) -> String {
     let mut out = String::new();
--- packages/std/src/query.rs
+++ packages/std/src/query.rs
@@ -1,35 +1,37 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use crate::api::ApiError;
+use crate::api::{ApiError, ApiResult};
 use crate::encoding::Binary;
 use crate::types::{Coin, HumanAddr};
 
 pub type QueryResponse = Binary;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum QueryResult {
-    Ok(QueryResponse),
-    Err(ApiError),
-}
+pub type QueryResult = ApiResult<QueryResponse, ApiError>;
 
-impl QueryResult {
-    // unwrap will panic on err, or give us the real data useful for tests
-    pub fn unwrap(self) -> QueryResponse {
-        match self {
-            QueryResult::Err(msg) => panic!("Unexpected error: {}", msg),
-            QueryResult::Ok(res) => res,
-        }
-    }
-
-    pub fn is_err(&self) -> bool {
-        match self {
-            QueryResult::Err(_) => true,
-            _ => false,
-        }
-    }
-}
+//#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+//#[serde(rename_all = "snake_case")]
+//pub enum QueryResult {
+//    Ok(QueryResponse),
+//    Err(ApiError),
+//}
+//
+//impl QueryResult {
+//    // unwrap will panic on err, or give us the real data useful for tests
+//    pub fn unwrap(self) -> QueryResponse {
+//        match self {
+//            QueryResult::Err(msg) => panic!("Unexpected error: {}", msg),
+//            QueryResult::Ok(res) => res,
+//        }
+//    }
+//
+//    pub fn is_err(&self) -> bool {
+//        match self {
+//            QueryResult::Err(_) => true,
+//            _ => false,
+//        }
+//    }
+//}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
