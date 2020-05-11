2e480a3f3da284b9aeeae27a0a48f226d2767c99
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-05-11 18:35:21 +0200

    Define messages and state for staking contract

--- contracts/staking/src/msg.rs
+++ contracts/staking/src/msg.rs
@@ -1,27 +1,90 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use cosmwasm_std::{Decimal9, HumanAddr, Uint128};
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
-    pub count: i32,
+    /// name of the derivative token (FIXME: auto-generate?)
+    pub name: String,
+    /// symbol / ticker of the derivative token
+    pub symbol: String,
+    /// decimal places of the derivative token (for UI)
+    pub decimals: u8,
+
+    /// This is the validator that all tokens will be bonded to
+    pub validator: HumanAddr,
+
+    /// this is how much the owner takes as a cut when someone unbonds
+    /// TODO
+    pub exit_tax: Decimal9,
+    /// This is the minimum amount we will pull out to reinvest, as well as a minumum
+    /// that can be unbonded (to avoid needless staking tx)
+    pub min_withdrawl: Uint128,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum HandleMsg {
-    Increment {},
-    Reset { count: i32 },
+    /// Transfer moves the derivative token
+    Transfer {
+        recipient: HumanAddr,
+        amount: Uint128,
+    },
+    /// Bond will bond all staking tokens sent with the message and release derivative tokens
+    Bond {},
+    /// Unbond will "burn" the given amount of derivative tokens and send the unbonded
+    /// staking tokens to the message sender (after exit tax is deducted)
+    Unbond { amount: Uint128 },
+    /// Reinvest will check for all accumulated rewards, withdraw them, and
+    /// re-bond them to the same validator. Anyone can call this, which updates
+    /// the value of the token (how much under custody).
+    Reinvest {},
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    /// Balance shows the number of staking derivatives
+    Balance { address: HumanAddr },
+    /// TokenInfo shows the metadata of the token for UIs
+    TokenInfo {},
+    /// Investment shows info on total staking tokens under custody,
+    /// with which validator, as well as how many derivative tokens are lists.
+    /// It also shows with the exit tax.
+    Investment {},
 }
 
-// We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct BalanceResponse {
+    pub balance: Uint128,
+}
+
+/// TokenInfoResponse is info to display the derivative token in a UI
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct TokenInfoResponse {
+    /// name of the derivative token
+    pub name: String,
+    /// symbol / ticker of the derivative token
+    pub symbol: String,
+    /// decimal places of the derivative token (for UI)
+    pub decimals: u8,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InvestmentResponse {
+    pub token_supply: Uint128,
+    pub staked_tokens: Coin,
+    // ratio of staked_tokens / token_supply (or how many native tokens that one derivative token is nominally worth)
+    pub nominal_value: Decimal9,
+
+    /// owner created the contract and takes a cut
+    pub owner: HumanAddr,
+    /// this is how much the owner takes as a cut when someone unbonds
+    pub exit_tax: Decimal9,
+    /// All tokens are bonded to this validator
+    pub validator: HumanAddr,
+    /// This is the minimum amount we will pull out to reinvest, as well as a minumum
+    /// that can be unbonded (to avoid needless staking tx)
+    pub min_withdrawl: Uint128,
 }
--- contracts/staking/src/state.rs
+++ contracts/staking/src/state.rs
@@ -1,21 +1,74 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+use cosmwasm_std::{CanonicalAddr, Decimal9, HumanAddr, ReadonlyStorage, Storage, Uint128};
+use cosmwasm_storage::{
+    bucket, bucket_read, singleton, singleton_read, Bucket, ReadonlyBucket, ReadonlySingleton,
+    Singleton,
+};
 
-pub static CONFIG_KEY: &[u8] = b"config";
+use crate::msg::TokenInfoResponse;
 
+pub const KEY_INVESTMENT: &[u8] = b"invest";
+pub const KEY_TOKEN_INFO: &[u8] = b"token";
+pub const KEY_TOTAL_SUPPLY: &[u8] = b"total_supply";
+
+pub const PREFIX_BALANCE: &[u8] = b"balance";
+
+pub fn balances<'a, S: Storage>(storage: &'a mut S) -> Bucket<'a, S, Uint128> {
+    bucket(PREFIX_BALANCE, storage)
+}
+
+pub fn balances_read<'a, S: ReadonlyStorage>(
+    storage: &'a S,
+) -> ReadonlyBucket<'a, S, InvestmentInfo> {
+    bucket_read(PREFIX_BALANCE, storage)
+}
+
+/// Investment info is fixed at initialization, and is used to control the function of the contract
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
+pub struct InvestmentInfo {
+    /// owner created the contract and takes a cut
     pub owner: CanonicalAddr,
+    /// this is how much the owner takes as a cut when someone unbonds
+    pub exit_tax: Decimal9,
+    /// All tokens are bonded to this validator
+    /// FIXME: humanize/canonicalize address doesn't work for validator addrresses
+    pub validator: HumanAddr,
+    /// This is the minimum amount we will pull out to reinvest, as well as a minumum
+    /// that can be unbonded (to avoid needless staking tx)
+    pub min_withdrawl: Uint128,
 }
 
-pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
-    singleton(storage, CONFIG_KEY)
+/// Supply is dynamic and tracks the current supply of staked and ERC20 tokens.
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Supply {
+    /// issued is how many derivative tokens this contract has issued
+    pub issued: Uint128,
+    /// bonded is how many native tokens exist bonded to the validator
+    pub bonded: Uint128,
 }
 
-pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
-    singleton_read(storage, CONFIG_KEY)
+pub fn invest_info<S: Storage>(storage: &mut S) -> Singleton<S, InvestmentInfo> {
+    singleton(storage, KEY_INVESTMENT)
+}
+
+pub fn invest_info_read<S: ReadonlyStorage>(storage: &S) -> ReadonlySingleton<S, InvestmentInfo> {
+    singleton_read(storage, KEY_INVESTMENT)
+}
+
+pub fn token_info<S: Storage>(storage: &mut S) -> Singleton<S, TokenInfoResponse> {
+    singleton(storage, KEY_TOKEN_INFO)
+}
+
+pub fn token_info_read<S: ReadonlyStorage>(storage: &S) -> ReadonlySingleton<S, TokenInfoResponse> {
+    singleton_read(storage, KEY_TOKEN_INFO)
+}
+
+pub fn total_supply<S: Storage>(storage: &mut S) -> Singleton<S, Supply> {
+    singleton(storage, KEY_TOTAL_SUPPLY)
+}
+
+pub fn total_supply_read<S: ReadonlyStorage>(storage: &S) -> ReadonlySingleton<S, Supply> {
+    singleton_read(storage, KEY_TOTAL_SUPPLY)
 }
