c098457a74260436660385af124bdcdec89496e4
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-05-12 12:50:16 +0200

    Update schema

--- contracts/staking/examples/schema.rs
+++ contracts/staking/examples/schema.rs
@@ -3,8 +3,11 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use staking::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
-use staking::state::State;
+use staking::msg::{
+    BalanceResponse, ClaimsResponse, HandleMsg, InitMsg, InvestmentResponse, QueryMsg,
+    TokenInfoResponse,
+};
+use staking::state::{InvestmentInfo, Supply};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -15,6 +18,10 @@ fn main() {
     export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CountResponse), &out_dir);
+    export_schema(&schema_for!(BalanceResponse), &out_dir);
+    export_schema(&schema_for!(ClaimsResponse), &out_dir);
+    export_schema(&schema_for!(InvestmentResponse), &out_dir);
+    export_schema(&schema_for!(TokenInfoResponse), &out_dir);
+    export_schema(&schema_for!(InvestmentInfo), &out_dir);
+    export_schema(&schema_for!(Supply), &out_dir);
 }
