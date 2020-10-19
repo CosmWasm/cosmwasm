4ddabec456a3449283ef27671eb70f8a34cd70fd
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-19 13:50:34 +0200

    Add ExternMut ExternRef types

--- packages/std/src/deps.rs
+++ packages/std/src/deps.rs
@@ -9,3 +9,43 @@ pub struct Deps<S: Storage, A: Api, Q: Querier> {
     pub api: A,
     pub querier: Q,
 }
+
+pub struct DepsMut<'a, S: Storage, A: Api, Q: Querier> {
+    pub storage: &'a mut S,
+    pub api: &'a A,
+    pub querier: &'a Q,
+}
+
+pub struct DepsRef<'a, S: Storage, A: Api, Q: Querier> {
+    pub storage: &'a S,
+    pub api: &'a A,
+    pub querier: &'a Q,
+}
+
+impl<S: Storage, A: Api, Q: Querier> Deps<S, A, Q> {
+    pub fn as_ref(&'_ self) -> DepsRef<'_, S, A, Q> {
+        DepsRef {
+            storage: &self.storage,
+            api: &self.api,
+            querier: &self.querier,
+        }
+    }
+
+    pub fn as_mut(&'_ mut self) -> DepsMut<'_, S, A, Q> {
+        DepsMut {
+            storage: &mut self.storage,
+            api: &self.api,
+            querier: &self.querier,
+        }
+    }
+}
+
+impl<'a> DepsMut<'a> {
+    pub fn as_ref(&'_ self) -> DepsRef<'_> {
+        DepsRef {
+            storage: &self.storage,
+            api: self.api,
+            querier: self.querier,
+        }
+    }
+}
