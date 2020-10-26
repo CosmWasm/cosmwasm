8a42dabe40be231f912799937a2937d6d1cb5830
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-10-26 18:53:29 +0100

    Remove TypedStorage and PrefixedStorage from the cosmwasm-storage

--- packages/storage/src/lib.rs
+++ packages/storage/src/lib.rs
@@ -1,17 +1,13 @@
 mod bucket;
 mod length_prefixed;
 mod namespace_helpers;
-mod prefixed_storage;
 mod sequence;
 mod singleton;
 mod transactions;
 mod type_helpers;
-mod typed;
 
 pub use bucket::{bucket, bucket_read, Bucket, ReadonlyBucket};
 pub use length_prefixed::{to_length_prefixed, to_length_prefixed_nested};
-pub use prefixed_storage::{prefixed, prefixed_read, PrefixedStorage, ReadonlyPrefixedStorage};
 pub use sequence::{currval, nextval, sequence};
 pub use singleton::{singleton, singleton_read, ReadonlySingleton, Singleton};
 pub use transactions::{transactional, RepLog, StorageTransaction};
-pub use typed::{typed, typed_read, ReadonlyTypedStorage, TypedStorage};
