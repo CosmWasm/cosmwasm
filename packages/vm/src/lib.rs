2bcd54a32c9b3360cbe5c0f19a12157e6564e829
Simon Warta <simon@warta.it>
2020-10-30 18:23:46 +0100

    Add Size type

--- packages/vm/src/lib.rs
+++ packages/vm/src/lib.rs
@@ -14,6 +14,7 @@ mod memory;
 mod middleware;
 mod modules;
 mod serde;
+mod size;
 pub mod testing;
 mod traits;
 
@@ -31,4 +32,5 @@ pub use crate::features::features_from_csv;
 pub use crate::ffi::{FfiError, FfiResult, GasInfo};
 pub use crate::instance::{GasReport, Instance, InstanceOptions};
 pub use crate::serde::{from_slice, to_vec};
+pub use crate::size::Size;
 pub use crate::traits::{Api, Extern, Querier, Storage};
