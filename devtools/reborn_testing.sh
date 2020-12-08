48730f35eef420eda5921cec8e0c4abaf1ec443b
Simon Warta <simon@warta.it>
2020-12-08 07:54:35 +0100

    Run all tests in local testing

--- devtools/reborn_testing.sh
+++ devtools/reborn_testing.sh
@@ -8,21 +8,7 @@ command -v shellcheck > /dev/null && shellcheck "$0"
   && cargo check --features iterator --tests \
   && cargo check --features cranelift --tests \
   && cargo check --features cranelift,iterator --tests \
-  && cargo test --features iterator calls:: \
-  && cargo test --features iterator checksum:: \
-  && cargo test --features iterator context:: \
-  && cargo test --features iterator conversion:: \
-  && cargo test --features iterator compatibility \
-  && cargo test --features iterator errors:: \
-  && cargo test --features iterator features:: \
-  && cargo test --features iterator ffi:: \
-  && cargo test --features iterator limiting_tunables:: \
-  && cargo test --features iterator memory:: \
-  && cargo test --features iterator modules:: \
-  && cargo test --features iterator imports:: \
-  && cargo test --features iterator testing:: \
-  && cargo test --features iterator instance:: \
-  && cargo test --features iterator cache:: \
+  && cargo test --features iterator \
   && cargo clippy --features iterator -- -D warnings)
 
 # Contracts
