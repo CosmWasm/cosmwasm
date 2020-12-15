7a0cadc10de643dc72721ded428df39d83fb5286
Simon Warta <simon@warta.it>
2020-12-16 00:37:28 +0100

    Remove temporary metering feature

--- packages/vm/src/cache.rs
+++ packages/vm/src/cache.rs
@@ -510,7 +510,7 @@ mod tests {
     }
 
     #[test]
-    #[cfg(feature = "metering")]
+    #[ignore] // fails due to the inability to handle gas depletion (https://github.com/wasmerio/wasmer/issues/1931)
     fn recovers_from_out_of_gas() {
         let mut cache = unsafe { Cache::new(make_testing_options()).unwrap() };
         let id = cache.save_wasm(CONTRACT).unwrap();
