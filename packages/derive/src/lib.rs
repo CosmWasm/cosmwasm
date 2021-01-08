8e78f350cfa1d3181791021569f5403bb2ef338a
Simon Warta <simon@warta.it>
2021-01-08 23:34:57 +0100

    Support a variable number of arguments in entry_point

--- packages/derive/src/lib.rs
+++ packages/derive/src/lib.rs
@@ -9,18 +9,27 @@ pub fn entry_point(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
     let cloned = item.clone();
     let function = parse_macro_input!(cloned as syn::ItemFn);
     let name = function.sig.ident.to_string();
+    // The first argument is `deps`, the rest is region pointers
+    let args = function.sig.inputs.len() - 1;
+
+    // E.g. "ptr0: u32, ptr1: u32, ptr2: u32,"
+    let typed_ptrs = (0..args).fold(String::new(), |acc, i| format!("{}ptr{}: u32,", acc, i));
+    // E.g. "ptr0, ptr1, ptr2,"
+    let ptrs = (0..args).fold(String::new(), |acc, i| format!("{}ptr{},", acc, i));
 
     let new_code = format!(
         r##"
         #[cfg(target_arch = "wasm32")]
         mod __wasm_export_{name} {{ // new module to avoid conflict of function name
             #[no_mangle]
-            extern "C" fn {name}(env_ptr: u32, info_ptr: u32, msg_ptr: u32) -> u32 {{
-                cosmwasm_std::do_{name}(&super::{name}, env_ptr, info_ptr, msg_ptr)
+            extern "C" fn {name}({typed_ptrs}) -> u32 {{
+                cosmwasm_std::do_{name}(&super::{name}, {ptrs})
             }}
         }}
     "##,
         name = name,
+        typed_ptrs = typed_ptrs,
+        ptrs = ptrs
     );
     let entry = TokenStream::from_str(&new_code).unwrap();
     item.extend(entry);
