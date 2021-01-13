b3948891a6cde9f6f5070a1edfed2ede99885ceb
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-01-13 20:54:34 +0100

    Finish implementing ibc handler logic

--- contracts/ibc-reflect/examples/schema.rs
+++ contracts/ibc-reflect/examples/schema.rs
@@ -1,9 +1,9 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for, export_schema_with_title};
 
-use ibc_reflect::msg::{HandleMsg, InitMsg};
+use ibc_reflect::msg::{HandleMsg, InitMsg, PacketMsg, AcknowledgementMsg};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -13,4 +13,6 @@ fn main() {
 
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(InitMsg), &out_dir);
+    export_schema(&schema_for!(PacketMsg), &out_dir);
+    export_schema_with_title(&mut schema_for!(AcknowledgementMsg), &out_dir, "AcknowledgementMsg");
 }
--- contracts/ibc-reflect/src/msg.rs
+++ contracts/ibc-reflect/src/msg.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::field_reassign_with_default)] // see https://github.com/CosmWasm/cosmwasm/issues/685
 
-use cosmwasm_std::HumanAddr;
+use cosmwasm_std::{HumanAddr, CosmosMsg, ContractResult};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -22,7 +22,24 @@ pub enum HandleMsg {
     },
 }
 
+/// This is the message we send to the reflect contract to initialize it
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct ReflectInitMsg {
     pub callback_id: Option<String>,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ReflectHandleMsg {
+    ReflectMsg { msgs: Vec<CosmosMsg> },
+}
+
+/// This is the format of the packets we expect to receive
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct PacketMsg {
+    pub msgs: Vec<CosmosMsg>,
+}
+
+/// This is the format of the packets we send on ack
+/// Just acknowledge success or error
+pub type AcknowledgementMsg = ContractResult<()>;
