3a9840f9f5ffb56b6eb8b2c387aae66d3ce520f1
Mauro Lacy <mauro@lacy.com.es>
2021-01-17 09:55:06 +0100

    Add description / improve syntax

--- packages/vm/bin/module_size.sh
+++ packages/vm/bin/module_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Uses valgrind's massif tool to compute heap memory consumption of compiled modules.
 
 WASM="$1"
 [ -z "$WASM" ] && echo "Usage: $0 <file>.wasm" && exit 1
@@ -9,7 +10,7 @@ SUM_UTIL="ms_print"
 PROG=`basename $0 .sh`
 BASE_DIR=`dirname $0`/..
 
-# Look for the useful info (compile_only result's)
+# Look for the useful info (compile_only() results)
 FN="compile_only"
 SRC="$BASE_DIR/src/bin/$PROG.rs"
 LINE=`grep -n $FN $SRC | tail -1 | cut -f1 -d:`
@@ -22,6 +23,6 @@ SUMMARY="$BASE_DIR/ms_print.log"
 $MEM_UTIL --massif-out-file=$RESULTS $BIN $WASM
 $SUM_UTIL $RESULTS >$SUMMARY
 
-# Compute compile_only total (heap) bytes
+# Compute compile_only() total (heap) bytes
 echo -n "module size (unserialized): "
 (tac $SUMMARY | sed -n '1,/^  n /p' | tac | grep "$PROG.rs:$LINE" | cut -f2 -d\( | cut -f1 -d\) | sed 's/,//g;s/B//' | sed ':a;N;s/\n/+/;ta' | bc -l | sed 's/$/ bytes/') && rm -f $RESULTS $SUMMARY
