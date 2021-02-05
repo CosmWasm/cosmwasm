ccd44ec606b3f30c2e0c4997c6c8dc99833d99ed
Mauro Lacy <mauro@lacy.com.es>
2021-02-05 18:24:08 +0100

    Add ed25519_verify impl / test

--- packages/vm/src/crypto.rs
+++ packages/vm/src/crypto.rs
@@ -1,26 +1,37 @@
-// #[derive(Debug)]
-// pub struct SignatureVerification {
-//     secp: Verifier,
-// }
-//
-// impl SignatureVerification {
-//     #[allow(dead_code)]
-//     pub fn new() -> SignatureVerification {
-//         SignatureVerification {
-//             secp: Verifier::new(),
-//         }
-//     }
-// }
-//
-// impl Default for SignatureVerification {
-//     fn default() -> Self {
-//         Self::new()
-//     }
-// }
+use ed25519_zebra::VerificationKey;
+
+use k256::{
+    ecdsa::signature::{DigestVerifier, Signature as _}, // traits
+    ecdsa::{Signature, VerifyingKey},                   // type aliases
+};
+use sha2::Digest; // trait
+
+use std::convert::TryFrom;
+
+use crate::errors::{VmError, VmResult};
+use crate::identity_digest::Identity256;
+
+pub fn ed25519_verify(
+    message_hash: &[u8],
+    signature_bytes: &[u8; 64],
+    public_key_bytes: &[u8; 32],
+) -> VmResult<()> {
+    // Deserialize
+    // let signature = signature_bytes.try_into().map_err(|err| VmError::crypto_err(err.to_string()))?;
+    let signature = (*signature_bytes).into();
+    VerificationKey::try_from(*public_key_bytes)
+        .and_then(|vk| vk.verify(&signature, &message_hash))
+        .map_err(|err| VmError::crypto_err(err.to_string()))
+}
 
 #[cfg(test)]
 mod tests {
-    use k256::{
+    use super::*;
+
+    use elliptic_curve::sec1::ToEncodedPoint;
+    use rand_core::OsRng;
+
+    use crate::crypto::ed25519_verify;
         ecdsa::signature::Signature as _,
         ecdsa::{signature::Signer, Signature, SigningKey},
         ecdsa::{signature::Verifier, VerifyingKey},
@@ -44,113 +55,32 @@ mod tests {
     const COSMOS_SIGNATURE_HEX2: &str = "525adc7e61565a509c60497b798c549fbf217bb5cd31b24cc9b419d098cc95330c99ecc4bc72448f85c365a4e3f91299a3d40412fb3751bab82f1940a83a0a4c";
     const COSMOS_SIGNATURE_HEX3: &str = "f3f2ca73806f2abbf6e0fe85f9b8af66f0e9f7f79051fdb8abe5bb8633b17da132e82d577b9d5f7a6dae57a144efc9ccc6eef15167b44b3b22a57240109762af";
 
-    // Test data originally from https://github.com/cosmos/cosmjs/blob/v0.24.0-alpha.22/packages/crypto/src/secp256k1.spec.ts#L195-L394
-    // const COSMOS_TESTS_JSON: &str = "./testdata/secp256k1_tests.json";
-
     #[test]
-    fn secp256k1_verify() {
+    fn test_ed25519_verify() {
+        use ed25519_zebra::SigningKey;
+
+        // Explicit hash
+        let message_hash = Sha256::new().chain(MSG).finalize();
+
         // Signing
-        let secret_key = SigningKey::random(&mut OsRng); // Serialize with `::to_bytes()`
+        let secret_key = SigningKey::new(&mut OsRng);
+        let signature = secret_key.sign(&message_hash);
 
-        // let hash = sha256::Hash::hash(&MSG.as_bytes);
-        // let signature: Signature = secret_key.sign_prehashed(MSG.as_bytes());
-
-        // Note: the signature type must be annotated or otherwise inferrable as
-        // `Signer` has many impls of the `Signer` trait (for both regular and
-        // recoverable signature types).
-        let signature: Signature = secret_key.sign(MSG.as_bytes()); // Message is internally digested :-/
+        // Types can be converted to raw byte arrays with From/Into
+        let public_key = VerificationKey::from(&secret_key);
 
         // Verification
-        let public_key = VerifyingKey::from(&secret_key); // Serialize with `::to_encoded_point()`
-        assert!(public_key.verify(MSG.as_bytes(), &signature).is_ok());
+        assert!(ed25519_verify(&message_hash, &signature.into(), &public_key.into()).is_ok());
 
         // Wrong message fails
-        let message_bad = [MSG, "\0"].concat();
-        assert!(public_key
-            .verify(message_bad.as_bytes(), &signature)
-            .is_err());
+        let bad_message_hash = Sha256::new().chain([MSG, "\0"].concat()).finalize();
+        assert!(ed25519_verify(&bad_message_hash, &signature.into(), &public_key.into()).is_err());
 
         // Other pubkey fails
-        let other_secret_key = SigningKey::random(&mut OsRng); // Serialize with `::to_bytes()`
-        let other_public_key = VerifyingKey::from(&other_secret_key); // Serialize with `::to_encoded_point()`
-        assert!(other_public_key.verify(MSG.as_bytes(), &signature).is_err());
+        let other_secret_key = SigningKey::new(&mut OsRng);
+        let other_public_key = VerificationKey::from(&other_secret_key);
+        assert!(
+            ed25519_verify(&message_hash, &signature.into(), &other_public_key.into()).is_err()
+        );
     }
-
-    #[test]
-    fn cosmos_secp256k1_verify() {
-        let public_key =
-            VerifyingKey::from_sec1_bytes(&base64::decode(COSMOS_PUBKEY_BASE64).unwrap()).unwrap();
-
-        for ((i, msg), sig) in (1..)
-            .zip(&[COSMOS_MSG_HEX1, COSMOS_MSG_HEX2, COSMOS_MSG_HEX3])
-            .zip(&[
-                COSMOS_SIGNATURE_HEX1,
-                COSMOS_SIGNATURE_HEX2,
-                COSMOS_SIGNATURE_HEX3,
-            ])
-        {
-            let message = hex::decode(msg).unwrap();
-            let signature = hex::decode(sig).unwrap();
-            let signature = Signature::from_bytes(signature.as_slice()).unwrap();
-
-            // Verify works
-            assert!(
-                public_key.verify(message.as_slice(), &signature).is_ok(),
-                format!("verify failed (test case {})", i)
-            );
-        }
-    }
-
-    /*
-    #[test]
-    fn cosmos_extra_secp256k1_verify() {
-        use std::fs::File;
-        use std::io::BufReader;
-
-        use serde::Deserialize;
-
-        #[derive(Deserialize, Debug)]
-        struct Encoded {
-            message: String,
-            message_hash: String,
-            signature: String,
-            #[serde(rename = "pubkey")]
-            public_key: String,
-        };
-
-        // Open the file in read-only mode with buffer.
-        let file = File::open(COSMOS_TESTS_JSON).unwrap();
-        let reader = BufReader::new(file);
-
-        let codes: Vec<Encoded> = serde_json::from_reader(reader).unwrap();
-
-        // Create our verification-only context
-        let crypto = SignatureVerification::new();
-
-        for (i, encoded) in (1..).zip(codes) {
-            let hash = hex::decode(&encoded.message_hash).unwrap();
-            let message_hash = sha256::Hash::hash(&hex::decode(&encoded.message).unwrap());
-            assert_eq!(hash.as_slice(), message_hash.into_inner());
-
-            let message = Message::from_slice(&message_hash).unwrap();
-
-            let public_key =
-                PublicKey::from_slice(&hex::decode(&encoded.public_key).unwrap()).unwrap();
-
-            let mut signature =
-                Signature::from_compact(&hex::decode(&encoded.signature).unwrap()).unwrap();
-            // Non low-S signatures require normalization
-            signature.normalize_s();
-
-            // Verify works
-            assert!(
-                crypto
-                    .secp
-                    .verify(&message, &signature, &public_key)
-                    .is_ok(),
-                format!("verify() failed (test case {})", i)
-            );
-        }
-    }
-    */
 }
