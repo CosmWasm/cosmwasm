07f72e1b1947d1803714b6fd86b7b5abc2396724
Mauro Lacy <mauro@lacy.com.es>
2021-02-06 11:08:42 +0100

    Remove unused dummy Sha224

--- packages/vm/src/dummy_sha2.rs
+++ packages/vm/src/dummy_sha2.rs
@@ -1,9 +1,9 @@
-//! Dummy Sha-256 and Sha-224 Digest impls.
-//! These digests store/accept an already hashed value of the proper length.
+//! Dummy Sha-256 Digest impl.
+//! This digest stores/accepts an already hashed value of the proper length.
 //! Just to comply with the Digest contract.
 //!
 //! Adapted from `sha2` [sha256.rs](https://github.com/RustCrypto/hashes/blob/master/sha2/src/sha256.rs)
-use sha2::digest::consts::{U28, U32};
+use sha2::digest::consts::U32;
 use sha2::digest::generic_array::GenericArray;
 use sha2::digest::{FixedOutputDirty, Reset, Update};
 
@@ -39,41 +39,6 @@ impl FixedOutputDirty for IdentitySha256 {
 }
 
 impl Reset for IdentitySha256 {
-    fn reset(&mut self) {
-        *self = Self::default();
-    }
-}
-
-/// The SHA-224 identity container.
-#[derive(Clone)]
-pub struct IdentitySha224 {
-    array: GenericArray<u8, U28>,
-}
-
-impl Default for IdentitySha224 {
-    fn default() -> Self {
-        IdentitySha224 {
-            array: GenericArray::default(),
-        }
-    }
-}
-
-impl Update for IdentitySha224 {
-    fn update(&mut self, hash: impl AsRef<[u8]>) {
-        assert_eq!(hash.as_ref().len(), 28);
-        self.array = *GenericArray::from_slice(hash.as_ref());
-    }
-}
-
-impl FixedOutputDirty for IdentitySha224 {
-    type OutputSize = U28;
-
-    fn finalize_into_dirty(&mut self, out: &mut digest::Output<Self>) {
-        *out = self.array;
-    }
-}
-
-impl Reset for IdentitySha224 {
     fn reset(&mut self) {
         *self = Self::default();
     }
