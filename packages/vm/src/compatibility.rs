b86dfdf6a7810dd49910f2d7bf78e5a25c8a2a34
Mauro Lacy <mauro@lacy.com.es>
2021-02-07 09:34:37 +0100

    Add verify_secp256k1 native

--- packages/vm/src/compatibility.rs
+++ packages/vm/src/compatibility.rs
@@ -15,6 +15,7 @@ const SUPPORTED_IMPORTS: &[&str] = &[
     "env.db_remove",
     "env.canonicalize_address",
     "env.humanize_address",
+    "env.verify_secp256k1",
     "env.debug",
     "env.query_chain",
     #[cfg(feature = "iterator")]
@@ -319,6 +320,7 @@ mod tests {
             (import "env" "db_remove" (func (param i32) (result i32)))
             (import "env" "canonicalize_address" (func (param i32 i32) (result i32)))
             (import "env" "humanize_address" (func (param i32 i32) (result i32)))
+            (import "env" "verify_secp256k1" (func (param i32 i32 i32) (result i32)))
         )"#,
         )
         .unwrap();
