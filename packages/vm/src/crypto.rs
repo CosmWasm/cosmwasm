ebb6c461d345e816867678e827f5ba64e409287d
Mauro Lacy <mauro@lacy.com.es>
2021-02-07 18:20:50 +0100

    Add Cosmos ed25519 signature tests

--- packages/vm/src/crypto.rs
+++ packages/vm/src/crypto.rs
@@ -55,6 +55,136 @@ mod tests {
     const COSMOS_SIGNATURE_HEX2: &str = "525adc7e61565a509c60497b798c549fbf217bb5cd31b24cc9b419d098cc95330c99ecc4bc72448f85c365a4e3f91299a3d40412fb3751bab82f1940a83a0a4c";
     const COSMOS_SIGNATURE_HEX3: &str = "f3f2ca73806f2abbf6e0fe85f9b8af66f0e9f7f79051fdb8abe5bb8633b17da132e82d577b9d5f7a6dae57a144efc9ccc6eef15167b44b3b22a57240109762af";
 
+    // Test data originally from https://github.com/cosmos/cosmjs/blob/v0.24.0-alpha.22/packages/crypto/src/secp256k1.spec.ts#L195-L394
+    const COSMOS_TESTS_JSON: &str = "./testdata/secp256k1_tests.json";
+
+    // TEST 1 from https://tools.ietf.org/html/rfc8032#section-7.1
+    const COSMOS_ED25519_MSG: &str = "";
+    const COSMOS_ED25519_PRIVATE_KEY_HEX: &str =
+        "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60";
+    const COSMOS_ED25519_PUBLIC_KEY_HEX: &str =
+        "d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a";
+    const COSMOS_ED25519_SIGNATURE_HEX: &str = "e5564300c360ac729086e2cc806e828a84877f1eb8e5d974d873e065224901555fb8821590a33bacc61e39701cf9b46bd25bf5f0595bbe24655141438e7a100b";
+
+    #[test]
+    fn test_secp256k1_verify() {
+        // Explicit / external hashing
+        let message_digest = Sha256::new().chain(MSG);
+        let message_hash = message_digest.clone().finalize();
+
+        // Signing
+        let secret_key = SigningKey::random(&mut OsRng); // Serialize with `::to_bytes()`
+
+        // Note: the signature type must be annotated or otherwise inferrable as
+        // `Signer` has many impls of the `Signer` trait (for both regular and
+        // recoverable signature types).
+        let signature: Signature = secret_key.sign_digest(message_digest);
+
+        let public_key = VerifyingKey::from(&secret_key); // Serialize with `::to_encoded_point()`
+
+        // Verification (uncompressed public key)
+        assert!(secp256k1_verify(
+            &message_hash,
+            signature.as_bytes(),
+            public_key.to_encoded_point(false).as_bytes()
+        )
+        .unwrap());
+
+        // Verification (compressed public key)
+        assert!(secp256k1_verify(
+            &message_hash,
+            signature.as_bytes(),
+            public_key.to_encoded_point(true).as_bytes()
+        )
+        .unwrap());
+
+        // Wrong message fails
+        let bad_message_hash = Sha256::new().chain([MSG, "\0"].concat()).finalize();
+        assert!(!secp256k1_verify(
+            &bad_message_hash,
+            signature.as_bytes(),
+            public_key.to_encoded_point(false).as_bytes()
+        )
+        .unwrap());
+
+        // Other pubkey fails
+        let other_secret_key = SigningKey::random(&mut OsRng);
+        let other_public_key = VerifyingKey::from(&other_secret_key);
+        assert!(!secp256k1_verify(
+            &message_hash,
+            signature.as_bytes(),
+            other_public_key.to_encoded_point(false).as_bytes()
+        )
+        .unwrap());
+    }
+
+    #[test]
+    fn test_cosmos_secp256k1_verify() {
+        let public_key = base64::decode(COSMOS_PUBKEY_BASE64).unwrap();
+
+        for ((i, msg), sig) in (1..)
+            .zip(&[COSMOS_MSG_HEX1, COSMOS_MSG_HEX2, COSMOS_MSG_HEX3])
+            .zip(&[
+                COSMOS_SIGNATURE_HEX1,
+                COSMOS_SIGNATURE_HEX2,
+                COSMOS_SIGNATURE_HEX3,
+            ])
+        {
+            let message = hex::decode(msg).unwrap();
+            let signature = hex::decode(sig).unwrap();
+
+            // Explicit hash
+            let message_hash = Sha256::new().chain(&message).finalize();
+
+            // secp256k1_verify works
+            assert!(
+                secp256k1_verify(&message_hash, &signature, &public_key).unwrap(),
+                format!("secp256k1_verify() failed (test case {})", i)
+            );
+        }
+    }
+
+    #[test]
+    fn test_cosmos_extra_secp256k1_verify() {
+        use std::fs::File;
+        use std::io::BufReader;
+
+        use serde::Deserialize;
+
+        #[derive(Deserialize, Debug)]
+        struct Encoded {
+            message: String,
+            message_hash: String,
+            signature: String,
+            #[serde(rename = "pubkey")]
+            public_key: String,
+        }
+
+        // Open the file in read-only mode with buffer.
+        let file = File::open(COSMOS_TESTS_JSON).unwrap();
+        let reader = BufReader::new(file);
+
+        let codes: Vec<Encoded> = serde_json::from_reader(reader).unwrap();
+
+        for (i, encoded) in (1..).zip(codes) {
+            let message = hex::decode(&encoded.message).unwrap();
+
+            let hash = hex::decode(&encoded.message_hash).unwrap();
+            let message_hash = Sha256::new().chain(&message).finalize();
+            assert_eq!(hash.as_slice(), message_hash.as_slice());
+
+            let signature = hex::decode(&encoded.signature).unwrap();
+
+            let public_key = hex::decode(&encoded.public_key).unwrap();
+
+            // secp256k1_verify() works
+            assert!(
+                secp256k1_verify(&message_hash, &signature, &public_key).unwrap(),
+                format!("verify() failed (test case {})", i)
+            );
+        }
+    }
+
     #[test]
     fn test_ed25519_verify() {
         let message = MSG.as_bytes();
@@ -80,5 +210,39 @@ mod tests {
         let other_public_key = ed25519::VerificationKey::from(&other_secret_key);
         let other_public_key_bytes: [u8; 32] = other_public_key.into();
         assert!(ed25519_verify(&message, &signature_bytes, &other_public_key_bytes).is_err());
+    }
+
+    #[test]
+    fn test_cosmos_ed25519_verify() {
+        let secret_key = ed25519::SigningKey::try_from(
+            hex::decode(COSMOS_ED25519_PRIVATE_KEY_HEX)
+                .unwrap()
+                .as_slice(),
+        )
+        .unwrap();
+        let public_key = ed25519::VerificationKey::try_from(
+            hex::decode(COSMOS_ED25519_PUBLIC_KEY_HEX)
+                .unwrap()
+                .as_slice(),
+        )
+        .unwrap();
+        let signature = secret_key.sign(&COSMOS_ED25519_MSG.as_bytes());
+
+        let signature_bytes: [u8; 64] = signature.into();
+        let public_key_bytes: [u8; 32] = public_key.into();
+
+        assert_eq!(
+            signature_bytes,
+            hex::decode(&COSMOS_ED25519_SIGNATURE_HEX)
+                .unwrap()
+                .as_slice()
+        );
+
+        assert!(ed25519_verify(
+            &COSMOS_ED25519_MSG.as_bytes(),
+            &signature_bytes,
+            &public_key_bytes
+        )
+        .is_ok());
     }
 }
