5bc28fcf3ccbca164c79385aa55017192a8d1fc6
Mauro Lacy <mauro@lacy.com.es>
2021-02-14 12:32:41 +0100

    Add ed25519 integration tests

--- contracts/crypto-verify/tests/integration.rs
+++ contracts/crypto-verify/tests/integration.rs
@@ -33,9 +33,14 @@ static WASM: &[u8] = include_bytes!("../target/wasm32-unknown-unknown/release/cr
 
 const CREATOR: &str = "creator";
 
-const MESSAGE_HEX: &str = "5c868fedb8026979ebd26f1ba07c27eedf4ff6d10443505a96ecaf21ba8c4f0937b3cd23ffdc3dd429d4cd1905fb8dbcceeff1350020e18b58d2ba70887baa3a9b783ad30d3fbf210331cdd7df8d77defa398cdacdfc2e359c7ba4cae46bb74401deb417f8b912a1aa966aeeba9c39c7dd22479ae2b30719dca2f2206c5eb4b7";
-const SIGNATURE_HEX: &str = "207082eb2c3dfa0b454e0906051270ba4074ac93760ba9e7110cd9471475111151eb0dbbc9920e72146fb564f99d039802bf6ef2561446eb126ef364d21ee9c4";
-const PUBLIC_KEY_HEX: &str = "04051c1ee2190ecfb174bfe4f90763f2b4ff7517b70a2aec1876ebcfd644c4633fb03f3cfbd94b1f376e34592d9d41ccaf640bb751b00a1fadeb0c01157769eb73";
+const SECP256K1_MESSAGE_HEX: &str = "5c868fedb8026979ebd26f1ba07c27eedf4ff6d10443505a96ecaf21ba8c4f0937b3cd23ffdc3dd429d4cd1905fb8dbcceeff1350020e18b58d2ba70887baa3a9b783ad30d3fbf210331cdd7df8d77defa398cdacdfc2e359c7ba4cae46bb74401deb417f8b912a1aa966aeeba9c39c7dd22479ae2b30719dca2f2206c5eb4b7";
+const SECP256K1_SIGNATURE_HEX: &str = "207082eb2c3dfa0b454e0906051270ba4074ac93760ba9e7110cd9471475111151eb0dbbc9920e72146fb564f99d039802bf6ef2561446eb126ef364d21ee9c4";
+const SECP256K1_PUBLIC_KEY_HEX: &str = "04051c1ee2190ecfb174bfe4f90763f2b4ff7517b70a2aec1876ebcfd644c4633fb03f3cfbd94b1f376e34592d9d41ccaf640bb751b00a1fadeb0c01157769eb73";
+
+const ED25519_MESSAGE_HEX: &str = "af82";
+const ED25519_SIGNATURE_HEX: &str = "6291d657deec24024827e69c3abe01a30ce548a284743a445e3680d7db5ac3ac18ff9b538d16f290ae67f760984dc6594a7c15e9716ed28dc027beceea1ec40a";
+const ED25519_PUBLIC_KEY_HEX: &str =
+    "fc51cd8e6218a1a38da47ed00230f0580816ed13ba3303ac5deb911548908025";
 
 fn setup() -> Instance<MockApi, MockStorage, MockQuerier> {
     let mut deps = mock_instance(WASM, &[]);
@@ -52,14 +57,14 @@ fn init_works() {
 }
 
 #[test]
-fn verify_works() {
+fn cosmos_signature_verify_works() {
     let mut deps = setup();
 
-    let message = hex::decode(MESSAGE_HEX).unwrap();
-    let signature = hex::decode(SIGNATURE_HEX).unwrap();
-    let public_key = hex::decode(PUBLIC_KEY_HEX).unwrap();
+    let message = hex::decode(SECP256K1_MESSAGE_HEX).unwrap();
+    let signature = hex::decode(SECP256K1_SIGNATURE_HEX).unwrap();
+    let public_key = hex::decode(SECP256K1_PUBLIC_KEY_HEX).unwrap();
 
-    let verify_msg = QueryMsg::VerifySignature {
+    let verify_msg = QueryMsg::VerifyCosmosSignature {
         message: Binary(message),
         signature: Binary(signature),
         public_key: Binary(public_key),
@@ -72,16 +77,16 @@ fn verify_works() {
 }
 
 #[test]
-fn verify_fails() {
+fn cosmos_signature_verify_fails() {
     let mut deps = setup();
 
-    let mut message = hex::decode(MESSAGE_HEX).unwrap();
+    let mut message = hex::decode(SECP256K1_MESSAGE_HEX).unwrap();
     // alter hash
     message[0] ^= 0x01;
-    let signature = hex::decode(SIGNATURE_HEX).unwrap();
-    let public_key = hex::decode(PUBLIC_KEY_HEX).unwrap();
+    let signature = hex::decode(SECP256K1_SIGNATURE_HEX).unwrap();
+    let public_key = hex::decode(SECP256K1_PUBLIC_KEY_HEX).unwrap();
 
-    let verify_msg = QueryMsg::VerifySignature {
+    let verify_msg = QueryMsg::VerifyCosmosSignature {
         message: Binary(message),
         signature: Binary(signature),
         public_key: Binary(public_key),
@@ -94,14 +99,73 @@ fn verify_fails() {
 }
 
 #[test]
-fn verify_errors() {
+#[should_panic(expected = "empty")]
+fn cosmos_signature_verify_panics() {
     let mut deps = setup();
 
-    let message = hex::decode(MESSAGE_HEX).unwrap();
-    let signature = hex::decode(SIGNATURE_HEX).unwrap();
+    let message = hex::decode(SECP256K1_MESSAGE_HEX).unwrap();
+    let signature = hex::decode(SECP256K1_SIGNATURE_HEX).unwrap();
     let public_key = vec![];
 
-    let verify_msg = QueryMsg::VerifySignature {
+    let verify_msg = QueryMsg::VerifyCosmosSignature {
+        message: Binary(message),
+        signature: Binary(signature),
+        public_key: Binary(public_key),
+    };
+    let _ = query(&mut deps, mock_env(), verify_msg).unwrap();
+}
+
+#[test]
+fn tendermint_signature_verify_works() {
+    let mut deps = setup();
+
+    let message = hex::decode(ED25519_MESSAGE_HEX).unwrap();
+    let signature = hex::decode(ED25519_SIGNATURE_HEX).unwrap();
+    let public_key = hex::decode(ED25519_PUBLIC_KEY_HEX).unwrap();
+
+    let verify_msg = QueryMsg::VerifyTendermintSignature {
+        message: Binary(message),
+        signature: Binary(signature),
+        public_key: Binary(public_key),
+    };
+
+    let raw = query(&mut deps, mock_env(), verify_msg).unwrap();
+    let res: VerifyResponse = from_slice(&raw).unwrap();
+
+    assert_eq!(res, VerifyResponse { verifies: true });
+}
+
+#[test]
+fn tendermint_signature_verify_fails() {
+    let mut deps = setup();
+
+    let mut message = hex::decode(ED25519_MESSAGE_HEX).unwrap();
+    // alter hash
+    message[0] ^= 0x01;
+    let signature = hex::decode(ED25519_SIGNATURE_HEX).unwrap();
+    let public_key = hex::decode(ED25519_PUBLIC_KEY_HEX).unwrap();
+
+    let verify_msg = QueryMsg::VerifyTendermintSignature {
+        message: Binary(message),
+        signature: Binary(signature),
+        public_key: Binary(public_key),
+    };
+
+    let raw = query(&mut deps, mock_env(), verify_msg).unwrap();
+    let res: VerifyResponse = from_slice(&raw).unwrap();
+
+    assert_eq!(res, VerifyResponse { verifies: false });
+}
+
+#[test]
+fn tendermint_signature_verify_errs() {
+    let mut deps = setup();
+
+    let message = hex::decode(ED25519_MESSAGE_HEX).unwrap();
+    let signature = hex::decode(ED25519_SIGNATURE_HEX).unwrap();
+    let public_key = vec![];
+
+    let verify_msg = QueryMsg::VerifyTendermintSignature {
         message: Binary(message),
         signature: Binary(signature),
         public_key: Binary(public_key),
@@ -122,7 +186,7 @@ fn query_works() {
     assert_eq!(
         res,
         ListVerificationsResponse {
-            verification_schemes: vec!["secp256k1".into()]
+            verification_schemes: vec!["secp256k1".into(), "ed25519".into()]
         }
     );
 }
