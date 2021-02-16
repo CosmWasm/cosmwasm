b14dcbec1ea9a1a62923782af65ff09174401712
Mauro Lacy <mauro@lacy.com.es>
2021-02-16 13:32:55 +0100

    Remove non_exhaustive, add "internal" comments / hidden directive

--- packages/crypto/src/lib.rs
+++ packages/crypto/src/lib.rs
@@ -4,6 +4,12 @@ mod crypto;
 mod errors;
 mod identity_digest;
 
+/// The crypto crate is intended to be used in internal crates / utils.
+/// Please don't use any of these types directly, as
+/// they might change frequently, or be removed in the future.
+#[doc(hidden)]
 pub use crate::crypto::secp256k1_verify;
+#[doc(hidden)]
 pub use crate::crypto::{MESSAGE_HASH_MAX_LENGTH, PUBKEY_MAX_LENGTH, SIGNATURE_MAX_LENGTH};
+#[doc(hidden)]
 pub use crate::errors::{CryptoError, CryptoResult};
