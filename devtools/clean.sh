5e6c8797c311683ff39f53df2bdd13b679eeade7
Mauro Lacy <mauro@lacy.com.es>
2021-03-09 22:32:50 +0100

    Rename workspace clean

--- devtools/clean.sh
+++ devtools/clean.sh
@@ -2,11 +2,6 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-for contract_dir in contracts/*/
-do
-  (cd "$contract_dir" && cargo clean)
-done
-
 (cd packages/std && cargo clean)
 (cd packages/crypto &&  cargo clean)
 (cd packages/storage &&  cargo clean)
