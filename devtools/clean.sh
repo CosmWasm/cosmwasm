b8d2c873254286c4812d915566c83f24bc46f2ed
Mauro Lacy <mauro@lacy.com.es>
2021-03-09 22:58:54 +0100

    Format clean scripts

--- devtools/clean.sh
+++ devtools/clean.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
 (cd packages/std && cargo clean)
-(cd packages/crypto &&  cargo clean)
-(cd packages/storage &&  cargo clean)
-(cd packages/schema &&  cargo clean)
-(cd packages/vm &&  cargo clean)
+(cd packages/crypto && cargo clean)
+(cd packages/storage && cargo clean)
+(cd packages/schema && cargo clean)
+(cd packages/vm && cargo clean)
