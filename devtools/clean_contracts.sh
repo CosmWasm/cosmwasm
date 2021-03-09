aeafc94dbda95d831fc1ec3d1a2545ea2a9fc984
Mauro Lacy <mauro@lacy.com.es>
2021-03-09 22:58:54 +0100

    Format clean scripts

--- devtools/clean_contracts.sh
+++ devtools/clean_contracts.sh
@@ -2,7 +2,6 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-for contract_dir in contracts/*/
-do
+for contract_dir in contracts/*/; do
   (cd "$contract_dir" && cargo clean)
 done
