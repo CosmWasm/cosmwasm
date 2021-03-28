78b00f9a7d3936e3fa86b5b12f1da37938cdf8c8
Simon Warta <simon@warta.it>
2021-03-28 18:14:58 +0200

    Run clippy on tests in check_workspace.sh

--- devtools/check_workspace.sh
+++ devtools/check_workspace.sh
@@ -3,8 +3,8 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
 cargo fmt
-(cd packages/crypto && cargo build && cargo test && cargo clippy -- -D warnings)
-(cd packages/std && cargo wasm-debug --features iterator && cargo test --features iterator && cargo clippy --features iterator -- -D warnings && cargo schema)
-(cd packages/storage && cargo build && cargo test --features iterator && cargo clippy --features iterator -- -D warnings)
-(cd packages/schema && cargo build && cargo test && cargo clippy -- -D warnings)
-(cd packages/vm && cargo build --features iterator,stargate && cargo test --features iterator,stargate && cargo clippy --features iterator,stargate -- -D warnings)
+(cd packages/crypto && cargo build && cargo test && cargo clippy --tests -- -D warnings)
+(cd packages/std && cargo wasm-debug --features iterator && cargo test --features iterator && cargo clippy --tests --features iterator -- -D warnings && cargo schema)
+(cd packages/storage && cargo build && cargo test --features iterator && cargo clippy --tests --features iterator -- -D warnings)
+(cd packages/schema && cargo build && cargo test && cargo clippy --tests -- -D warnings)
+(cd packages/vm && cargo build --features iterator,stargate && cargo test --features iterator,stargate && cargo clippy --tests --features iterator,stargate -- -D warnings)
