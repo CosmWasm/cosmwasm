b4e42ff5802b465c42f66c578802579c6a52e6e2
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-04-21 12:50:24 +0200

    impl Display for Empty

--- packages/std/src/results/empty.rs
+++ packages/std/src/results/empty.rs
@@ -1,5 +1,6 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 /// An empty struct that serves as a placeholder in different places,
 /// such as contracts that don't set a custom message.
@@ -7,8 +8,15 @@ use serde::{Deserialize, Serialize};
 /// It is designed to be expressable in correct JSON and JSON Schema but
 /// contains no meaningful data. Previously we used enums without cases,
 /// but those cannot represented as valid JSON Schema (https://github.com/CosmWasm/cosmwasm/issues/451)
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct Empty {}
+
+/// Some uses require Display on contract input, error. Allow use of Empty there.
+impl fmt::Display for Empty {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("{}")
+    }
+}
 
 #[cfg(test)]
 mod tests {
