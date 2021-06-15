84cadd909111e71421066847f7ff1a175920063b
Simon Warta <simon@warta.it>
2021-06-15 10:47:11 +0200

    Remove obsolete comments

--- packages/vm/src/wasm_backend/compile.rs
+++ packages/vm/src/wasm_backend/compile.rs
@@ -1,11 +1,9 @@
-// use wasmer_middleware_common::metering;
 use wasmer::Module;
 
 use crate::errors::VmResult;
 use crate::size::Size;
 
 use super::store::make_compile_time_store;
-// use crate::middleware::DeterministicMiddleware;
 
 /// Compiles a given Wasm bytecode into a module.
 /// The given memory limit (in bytes) is used when memories are created.
