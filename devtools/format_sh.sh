4258c1c91cda3190671ec0a993e3abca63b5f894
Tomasz Kurcz <uint@lavabit.com>
2021-06-16 15:36:16 +0200

    Add the `-c` flag to formatting scripts

--- devtools/format_sh.sh
+++ devtools/format_sh.sh
@@ -2,4 +2,13 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-shfmt -w devtools packages
+# Running with -c makes the script only validate instead of editing in place.
+op="w"
+while getopts c option; do
+  case "${option}" in
+
+  c) op="d" ;;
+  esac
+done
+
+shfmt -$op devtools packages
