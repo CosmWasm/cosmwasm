6ba2262ceac597c946926415efa159329ddb5c50
Giancarlos Salas <giansalex@gmail.com>
2021-07-20 12:38:41 -0500

    Run format on std

--- packages/std/src/results/mod.rs
+++ packages/std/src/results/mod.rs
@@ -11,11 +11,11 @@ mod system_result;
 
 pub use attribute::{attr, Attribute};
 pub use contract_result::ContractResult;
-#[cfg(feature = "stargate")]
-pub use cosmos_msg::{GovMsg, VoteOption};
 pub use cosmos_msg::{wasm_execute, wasm_instantiate, BankMsg, CosmosMsg, WasmMsg};
 #[cfg(feature = "staking")]
 pub use cosmos_msg::{DistributionMsg, StakingMsg};
+#[cfg(feature = "stargate")]
+pub use cosmos_msg::{GovMsg, VoteOption};
 pub use empty::Empty;
 pub use query::QueryResponse;
 pub use response::Response;
