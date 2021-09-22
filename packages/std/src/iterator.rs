3b12865fb4d29b966699c22938d2bb296cfa5dad
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-09-22 20:38:58 +0200

    Make Pair more extensible

--- packages/std/src/iterator.rs
+++ packages/std/src/iterator.rs
@@ -2,7 +2,8 @@ use crate::errors::StdError;
 use std::convert::TryFrom;
 
 /// A Key-Value pair, returned from our iterators
-pub type Pair<V = Vec<u8>> = (Vec<u8>, V);
+/// (since it is less common to use, and never used with default V, we place K second)
+pub type Pair<V = Vec<u8>, K = Vec<u8>> = (K, V);
 
 #[derive(Copy, Clone)]
 // We assign these to integers to provide a stable API for passing over FFI (to wasm and Go)
@@ -26,5 +27,18 @@ impl TryFrom<i32> for Order {
 impl From<Order> for i32 {
     fn from(original: Order) -> i32 {
         original as _
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Pair;
+
+    #[test]
+    // make sure we add generic K without breaking existing code
+    fn ensure_pair_backwards_compatible() {
+        let _default: Pair = (vec![1, 2, 3], vec![5]);
+        let _value: Pair<u64> = (vec![4, 3], 1234567890);
+        let _with_key: Pair<u64, String> = ("hello".to_owned(), 12345678);
     }
 }
