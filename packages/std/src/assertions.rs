de4b35bd553d6e263099e0df4174409ce918d877
Simon Warta <simon@warta.it>
2021-10-07 17:11:08 +0200

    Add ensure_ne and doc testing

--- packages/std/src/assertions.rs
+++ packages/std/src/assertions.rs
@@ -2,13 +2,30 @@
 //! The methods in here never panic but return errors instead.
 
 /// Quick check for a guard. If the condition (first argument) is false,
-/// then return the second argument wrapped in Err(x).
+/// then return the second argument `x` wrapped in `Err(x)`.
 ///
-///   ensure!(permissions.delegate, ContractError::DelegatePerm {});
-///  is the same as
-///   if !permissions.delegate {
-///     return Err(ContractError::DelegatePerm {});
-///   }
+/// ```
+/// # enum ContractError {
+/// #     DelegatePerm {},
+/// # }
+/// #
+/// # struct Permissions {
+/// #     delegate: bool,
+/// # }
+/// #
+/// # fn body() -> Result<(), ContractError> {
+/// # let permissions = Permissions { delegate: true };
+/// use cosmwasm_std::ensure;
+/// ensure!(permissions.delegate, ContractError::DelegatePerm {});
+///
+/// // is the same as
+///
+/// if !permissions.delegate {
+///   return Err(ContractError::DelegatePerm {});
+/// }
+/// # Ok(())
+/// # }
+/// ```
 #[macro_export]
 macro_rules! ensure {
     ($cond:expr, $e:expr) => {
@@ -18,18 +35,73 @@ macro_rules! ensure {
     };
 }
 
-/// Quick check for a guard. Like assert_eq!, but rather than panic,
-/// it returns the second argument wrapped in Err(x).
+/// Quick check for a guard. Like `assert_eq!`, but rather than panic,
+/// it returns the third argument `x` wrapped in `Err(x)`.
 ///
-///   ensure_eq!(info.sender, cfg.admin, ContractError::Unauthorized {});
-///  is the same as
-///   if info.sender != cfg.admin {
-///     return Err(ContractError::Unauthorized {});
-///   }
+/// ```
+/// # use cosmwasm_std::{MessageInfo, Addr};
+/// #
+/// # enum ContractError {
+/// #     Unauthorized {},
+/// # }
+/// # struct Config {
+/// #     admin: String,
+/// # }
+/// #
+/// # fn body() -> Result<(), ContractError> {
+/// # let info = MessageInfo { sender: Addr::unchecked("foo"), funds: Vec::new() };
+/// # let cfg = Config { admin: "foo".to_string() };
+/// use cosmwasm_std::ensure_eq;
+///
+/// ensure_eq!(info.sender, cfg.admin, ContractError::Unauthorized {});
+///
+/// // is the same as
+///
+/// if info.sender != cfg.admin {
+///   return Err(ContractError::Unauthorized {});
+/// }
+/// # Ok(())
+/// # }
+/// ```
 #[macro_export]
 macro_rules! ensure_eq {
     ($a:expr, $b:expr, $e:expr) => {
-        ensure!($a == $b, $e);
+        // Not implemented via `ensure!` because the caller would have to import both macros.
+        if !($a == $b) {
+            return Err(std::convert::From::from($e));
+        }
+    };
+}
+
+/// Quick check for a guard. Like `assert_ne!`, but rather than panic,
+/// it returns the third argument `x` wrapped in Err(x).
+///
+/// ```
+/// # enum ContractError {
+/// #     NotAVoter {},
+/// # }
+/// #
+/// # fn body() -> Result<(), ContractError> {
+/// # let voting_power = 123;
+/// use cosmwasm_std::ensure_ne;
+///
+/// ensure_ne!(voting_power, 0, ContractError::NotAVoter {});
+///
+/// // is the same as
+///
+/// if voting_power != 0 {
+///   return Err(ContractError::NotAVoter {});
+/// }
+/// # Ok(())
+/// # }
+/// ```
+#[macro_export]
+macro_rules! ensure_ne {
+    ($a:expr, $b:expr, $e:expr) => {
+        // Not implemented via `ensure!` because the caller would have to import both macros.
+        if !($a != $b) {
+            return Err(std::convert::From::from($e));
+        }
     };
 }
 
@@ -108,5 +180,30 @@ mod tests {
         }
 
         let _err = check().unwrap_err();
+    }
+
+    #[test]
+    fn ensure_ne_works() {
+        let check = |a, b| {
+            ensure_ne!(a, b, StdError::generic_err("foobar"));
+            Ok(())
+        };
+
+        let err = check("123", "123").unwrap_err();
+        assert!(matches!(err, StdError::GenericErr { .. }));
+        check("123", "456").unwrap();
+    }
+
+    #[test]
+    fn ensure_ne_gets_precedence_right() {
+        // If this was expanded to `true || false == false` we'd get equality.
+        // It must be expanded to `(true || false) == false` and we expect inequality.
+
+        fn check() -> Result<(), StdError> {
+            ensure_ne!(true || false, false, StdError::generic_err("foobar"));
+            Ok(())
+        }
+
+        check().unwrap();
     }
 }
