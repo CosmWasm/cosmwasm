9bb4056f8383bc805b205276a7becff4d20c8528
Tomasz Kurcz <uint@lavabit.com>
2021-10-12 18:10:39 +0200

    Fix broken cosmos links

--- packages/std/src/results/events.rs
+++ packages/std/src/results/events.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 /// which then get magically converted to bytes for Tendermint somewhere between
 /// the Rust-Go interface, JSON deserialization and the `NewEvent` call in Cosmos SDK.
 ///
-/// [*Cosmos SDK* event]: https://docs.cosmos.network/v0.42/core/events.html
+/// [*Cosmos SDK* event]: https://docs.cosmos.network/master/core/events.html
 /// [*Cosmos SDK* StringEvent]: https://github.com/cosmos/cosmos-sdk/blob/v0.42.5/proto/cosmos/base/abci/v1beta1/abci.proto#L56-L70
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[non_exhaustive]
@@ -19,7 +19,7 @@ pub struct Event {
     ///
     /// You can learn more about these from [*Cosmos SDK* docs].
     ///
-    /// [*Cosmos SDK* docs]: https://docs.cosmos.network/v0.42/core/events.html
+    /// [*Cosmos SDK* docs]: https://docs.cosmos.network/master/core/events.html
     pub attributes: Vec<Attribute>,
 }
 
