b0ea918edb5468c2491c60621029ccc319779e70
Mauro Lacy <mauro@lacy.com.es>
2021-12-08 21:04:24 +0100

    Adjust gas values

--- packages/vm/src/instance.rs
+++ packages/vm/src/instance.rs
@@ -699,7 +699,7 @@ mod tests {
 
         let report2 = instance.create_gas_report();
         assert_eq!(report2.used_externally, 73);
-        assert_eq!(report2.used_internally, 5863800110);
+        assert_eq!(report2.used_internally, 5853750110);
         assert_eq!(report2.limit, LIMIT);
         assert_eq!(
             report2.remaining,
@@ -888,7 +888,7 @@ mod tests {
             .unwrap();
 
         let init_used = orig_gas - instance.get_gas_left();
-        assert_eq!(init_used, 5863800183);
+        assert_eq!(init_used, 5853750183);
     }
 
     #[test]
@@ -911,7 +911,7 @@ mod tests {
             .unwrap();
 
         let execute_used = gas_before_execute - instance.get_gas_left();
-        assert_eq!(execute_used, 8805403606);
+        assert_eq!(execute_used, 8785603606);
     }
 
     #[test]
@@ -945,6 +945,6 @@ mod tests {
         assert_eq!(answer.as_slice(), b"{\"verifier\":\"verifies\"}");
 
         let query_used = gas_before_query - instance.get_gas_left();
-        assert_eq!(query_used, 4598550006);
+        assert_eq!(query_used, 4596300006);
     }
 }
