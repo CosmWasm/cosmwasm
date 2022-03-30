bb33b26b2d7068cfc7a56645dc0053cbe36fa7c6
Simon Warta <simon@warta.it>
2022-03-30 15:04:11 +0200

    Use std::mem::transmute

--- packages/std/src/math/uint256.rs
+++ packages/std/src/math/uint256.rs
@@ -122,42 +122,7 @@ impl Uint256 {
             (self.0).0[1].to_be_bytes(),
             (self.0).0[0].to_be_bytes(),
         ];
-
-        // In Rust 1.56+ we can use `unsafe { std::mem::transmute::<[[u8; 8]; 4], [u8; 32]>(words) }` for this
-        [
-            words[0][0],
-            words[0][1],
-            words[0][2],
-            words[0][3],
-            words[0][4],
-            words[0][5],
-            words[0][6],
-            words[0][7],
-            words[1][0],
-            words[1][1],
-            words[1][2],
-            words[1][3],
-            words[1][4],
-            words[1][5],
-            words[1][6],
-            words[1][7],
-            words[2][0],
-            words[2][1],
-            words[2][2],
-            words[2][3],
-            words[2][4],
-            words[2][5],
-            words[2][6],
-            words[2][7],
-            words[3][0],
-            words[3][1],
-            words[3][2],
-            words[3][3],
-            words[3][4],
-            words[3][5],
-            words[3][6],
-            words[3][7],
-        ]
+        unsafe { std::mem::transmute::<[[u8; 8]; 4], [u8; 32]>(words) }
     }
 
     /// Returns a copy of the number as little endian bytes.
@@ -168,42 +133,7 @@ impl Uint256 {
             (self.0).0[2].to_le_bytes(),
             (self.0).0[3].to_le_bytes(),
         ];
-
-        // In Rust 1.56+ we can use `unsafe { std::mem::transmute::<[[u8; 8]; 4], [u8; 32]>(words) }` for this
-        [
-            words[0][0],
-            words[0][1],
-            words[0][2],
-            words[0][3],
-            words[0][4],
-            words[0][5],
-            words[0][6],
-            words[0][7],
-            words[1][0],
-            words[1][1],
-            words[1][2],
-            words[1][3],
-            words[1][4],
-            words[1][5],
-            words[1][6],
-            words[1][7],
-            words[2][0],
-            words[2][1],
-            words[2][2],
-            words[2][3],
-            words[2][4],
-            words[2][5],
-            words[2][6],
-            words[2][7],
-            words[3][0],
-            words[3][1],
-            words[3][2],
-            words[3][3],
-            words[3][4],
-            words[3][5],
-            words[3][6],
-            words[3][7],
-        ]
+        unsafe { std::mem::transmute::<[[u8; 8]; 4], [u8; 32]>(words) }
     }
 
     pub const fn is_zero(&self) -> bool {
