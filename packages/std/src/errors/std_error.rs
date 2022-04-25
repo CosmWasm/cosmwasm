573ec39f7a60ec38ec80fc01ae01f328a88de0a3
Jakub Bogucki <jakub@confio.gmbh>
2022-04-25 14:46:02 +0200

    Uint64/128/256 checked multiply implementation

--- packages/std/src/errors/std_error.rs
+++ packages/std/src/errors/std_error.rs
@@ -526,6 +526,15 @@ impl DivideByZeroError {
     }
 }
 
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum CheckedMultiplyRatioError {
+    #[error("Denominator must not be zero")]
+    DivideByZero {},
+
+    #[error("Multiplication overflow")]
+    MulOverflow {},
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
