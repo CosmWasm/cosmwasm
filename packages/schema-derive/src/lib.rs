f793c2416fd153fe5e88ee978df0373e62ce2565
Tomasz Kurcz <uint@lavabit.com>
2022-06-27 12:37:54 +0200

    schema: derive macro for QueryResponses

---
+++ packages/schema-derive/src/lib.rs
@@ -0,0 +1,42 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemEnum, Type, Variant};
+
+/// Extract the query -> response mapping out of an enum variant.
+fn parse_query(v: Variant) -> TokenStream {
+    let query = stringify!(v.ident);
+    let response_ty: Type = v
+        .attrs
+        .iter()
+        .find(|a| a.path.get_ident().unwrap() == "returns")
+        .unwrap()
+        .parse_args()
+        .unwrap();
+
+    quote! {
+        (#query, cosmwasm_schema::schema_for!(#response_ty))
+    }
+}
+
+#[proc_macro_derive(QueryResponses, attributes(returns))]
+pub fn query_responses_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as ItemEnum);
+    let ident = input.ident;
+
+    let responses = input.variants.into_iter().map(parse_query);
+
+    let expanded = quote! {
+        #[automatically_derived]
+        impl cosmwasm_schema::QueryResponses for #ident {
+            fn query_responses() -> std::collections::BTreeMap<&'static str, schemars::schema::RootSchema> {
+                [
+                    #( #responses, )*
+                ]
+                    .into_iter()
+                    .collect()
+            }
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
--- packages/schema/src/query_response.rs
+++ packages/schema/src/query_response.rs
@@ -2,6 +2,8 @@ use std::collections::BTreeMap;
 
 use schemars::schema::RootSchema;
 
+pub use cosmwasm_schema_derive::QueryResponses;
+
 pub trait QueryResponses {
     fn query_responses() -> BTreeMap<&'static str, RootSchema>;
 }
