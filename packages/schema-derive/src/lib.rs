17b6308e119813e77367eff4dbd98585eee308db
Tomasz Kurcz <uint@lavabit.com>
2022-06-29 18:46:00 +0200

    schema-derive: more tests

--- packages/schema-derive/src/lib.rs
+++ packages/schema-derive/src/lib.rs
@@ -1,9 +1,8 @@
-use proc_macro2::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, ItemEnum, Type, Variant};
+use quote::ToTokens;
+use syn::{parse_macro_input, parse_quote, ExprTuple, ItemEnum, ItemImpl, Type, Variant};
 
 /// Extract the query -> response mapping out of an enum variant.
-fn parse_query(v: Variant) -> TokenStream {
+fn parse_query(v: Variant) -> ExprTuple {
     let query = to_snake_case(&v.ident.to_string());
     let response_ty: Type = v
         .attrs
@@ -13,7 +12,7 @@ fn parse_query(v: Variant) -> TokenStream {
         .parse_args()
         .unwrap();
 
-    quote! {
+    parse_quote! {
         (#query.to_string(), cosmwasm_schema::schema_for!(#response_ty))
     }
 }
@@ -33,11 +32,18 @@ fn to_snake_case(input: &str) -> String {
 #[proc_macro_derive(QueryResponses, attributes(returns))]
 pub fn query_responses_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as ItemEnum);
+
+    let expanded = query_responses_derive_impl(input).into_token_stream();
+
+    proc_macro::TokenStream::from(expanded)
+}
+
+fn query_responses_derive_impl(input: ItemEnum) -> ItemImpl {
     let ident = input.ident;
 
     let responses = input.variants.into_iter().map(parse_query);
 
-    let expanded = quote! {
+    parse_quote! {
         #[automatically_derived]
         #[cfg(not(target_arch = "wasm32"))]
         impl cosmwasm_schema::QueryResponses for #ident {
@@ -47,43 +53,117 @@ pub fn query_responses_derive(input: proc_macro::TokenStream) -> proc_macro::Tok
                 ])
             }
         }
-    };
-
-    proc_macro::TokenStream::from(expanded)
+    }
 }
 
-#[cfg(tests)]
+#[cfg(test)]
 mod tests {
     use syn::parse_quote;
 
     use super::*;
 
     #[test]
-    fn parse_query() {
-        let variant = parse_quote! {
-            #[returns(Foo)]
-            GetFoo {},
+    fn happy_path() {
+        let input: ItemEnum = parse_quote! {
+            #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, QueryResponses)]
+            #[serde(rename_all = "snake_case")]
+            pub enum QueryMsg {
+                #[returns(SomeType)]
+                Balance {},
+                #[returns(some_crate::AnotherType)]
+                Supply {},
+            }
         };
 
         assert_eq!(
-            parse_query(variant),
-            quote! { ("get_foo", cosmwasm_schema::schema_for!(Foo)) }
-        );
-
-        let variant = parse_quote! {
-            #[returns(some_crate::Foo)]
-            GetFoo {},
-        };
-
-        assert_eq!(
-            parse_query(variant),
-            quote! { ("get_foo", cosmwasm_schema::schema_for!(some_crate::Foo)) }
+            query_responses_derive_impl(input),
+            parse_quote! {
+                #[automatically_derived]
+                #[cfg(not(target_arch = "wasm32"))]
+                impl cosmwasm_schema::QueryResponses for QueryMsg {
+                    fn response_schemas() -> std::collections::BTreeMap<String, schemars::schema::RootSchema> {
+                        std::collections::BTreeMap::from([
+                            ("balance".to_string(), cosmwasm_schema::schema_for!(SomeType)),
+                            ("supply".to_string(), cosmwasm_schema::schema_for!(some_crate::AnotherType)),
+                        ])
+                    }
+                }
+            }
         );
     }
 
     #[test]
-    fn to_snake_case() {
+    fn parse_query_works() {
+        let variant = parse_quote! {
+            #[returns(Foo)]
+            GetFoo {}
+        };
+
+        assert_eq!(
+            parse_query(variant),
+            parse_quote! {
+                ("get_foo".to_string(), cosmwasm_schema::schema_for!(Foo))
+            }
+        );
+
+        let variant = parse_quote! {
+            #[returns(some_crate::Foo)]
+            GetFoo {}
+        };
+
+        assert_eq!(
+            parse_query(variant),
+            parse_quote! { ("get_foo".to_string(), cosmwasm_schema::schema_for!(some_crate::Foo)) }
+        );
+    }
+
+    #[test]
+    fn to_snake_case_works() {
         assert_eq!(to_snake_case("SnakeCase"), "snake_case");
         assert_eq!(to_snake_case("Wasm123AndCo"), "wasm123_and_co");
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_if_queries_are_not_snake_case1() {
+        let input: ItemEnum = parse_quote! {
+            #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, QueryResponses)]
+            pub enum QueryMsg {
+                #[returns(SomeType)]
+                Balance {},
+            }
+        };
+
+        query_responses_derive_impl(input);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_if_queries_are_not_snake_case2() {
+        let input: ItemEnum = parse_quote! {
+            #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, QueryResponses)]
+            #[serde(deny_unknown_fields)]
+            pub enum QueryMsg {
+                #[returns(SomeType)]
+                Balance {},
+            }
+        };
+
+        query_responses_derive_impl(input);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_if_queries_are_not_snake_case3() {
+        let input: ItemEnum = parse_quote! {
+            #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema, QueryResponses)]
+            #[serde(rename_all = "kebab-case")]
+            pub enum QueryMsg {
+                #[returns(SomeType)]
+                Balance {},
+            }
+        };
+
+        query_responses_derive_impl(input);
     }
 }
