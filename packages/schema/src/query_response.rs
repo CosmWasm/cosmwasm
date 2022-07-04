23e638bb608a50ce0c1de31e6dda2f9cf695f2ba
Tomasz Kurcz <uint@lavabit.com>
2022-07-04 20:55:29 +0200

    schema: better errors for the integrity check

--- packages/schema/src/query_response.rs
+++ packages/schema/src/query_response.rs
@@ -1,11 +1,12 @@
 use std::collections::BTreeMap;
 
 use schemars::{schema::RootSchema, JsonSchema};
+use thiserror::Error;
 
 pub use cosmwasm_schema_derive::QueryResponses;
 
 pub trait QueryResponses: JsonSchema {
-    fn response_schemas() -> BTreeMap<String, RootSchema> {
+    fn response_schemas() -> Result<BTreeMap<String, RootSchema>, IntegrityError> {
         let response_schemas = Self::response_schemas_impl();
 
         let queries: Vec<_> = response_schemas
@@ -13,37 +14,59 @@ pub trait QueryResponses: JsonSchema {
             .map(std::borrow::Borrow::borrow)
             .collect();
 
-        check_api_integrity::<Self>(&queries);
+        check_api_integrity::<Self>(&queries)?;
 
-        response_schemas
+        Ok(response_schemas)
     }
 
     fn response_schemas_impl() -> BTreeMap<String, RootSchema>;
 }
 
 /// `generated_queries` is expected to be a sorted slice here!
-fn check_api_integrity<T: QueryResponses + ?Sized>(generated_queries: &[&str]) {
+fn check_api_integrity<T: QueryResponses + ?Sized>(
+    generated_queries: &[&str],
+) -> Result<(), IntegrityError> {
     let schema = crate::schema_for!(T);
+
+    // something more readable below?
+
     let mut schema_queries: Vec<_> = schema
         .schema
         .subschemas
-        .unwrap()
+        .ok_or(IntegrityError::InvalidQueryMsgSchema)?
         .one_of
-        .unwrap()
+        .ok_or(IntegrityError::InvalidQueryMsgSchema)?
         .into_iter()
         .map(|s| {
             s.into_object()
                 .object
-                .unwrap()
+                .ok_or(IntegrityError::InvalidQueryMsgSchema)?
                 .required
                 .into_iter()
                 .next()
-                .unwrap()
+                .ok_or(IntegrityError::InvalidQueryMsgSchema)
         })
-        .collect();
+        .collect::<Result<_, _>>()?;
     schema_queries.sort();
     if schema_queries != generated_queries {
-        // TODO: errors
-        panic!("NOES");
+        return Err(IntegrityError::InconsistentQueries {
+            query_msg: schema_queries,
+            responses: generated_queries.iter().map(ToString::to_string).collect(),
+        });
     }
+
+    Ok(())
+}
+
+#[derive(Debug, Error)]
+pub enum IntegrityError {
+    #[error("the structure of the QueryMsg schema was unexpected")]
+    InvalidQueryMsgSchema,
+    #[error(
+        "inconsistent queries - QueryMsg schema has {query_msg:?}, but query responses have {responses:?}"
+    )]
+    InconsistentQueries {
+        query_msg: Vec<String>,
+        responses: Vec<String>,
+    },
 }
