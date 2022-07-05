c0e2c07d77f61f2402836466efc1b4511997e884
Tomasz Kurcz <uint@lavabit.com>
2022-07-05 14:42:06 +0200

    schema: QueryResponse tests

--- packages/schema/src/query_response.rs
+++ packages/schema/src/query_response.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use schemars::{schema::RootSchema, JsonSchema};
 use thiserror::Error;
@@ -9,12 +9,9 @@ pub trait QueryResponses: JsonSchema {
     fn response_schemas() -> Result<BTreeMap<String, RootSchema>, IntegrityError> {
         let response_schemas = Self::response_schemas_impl();
 
-        let queries: Vec<_> = response_schemas
-            .keys()
-            .map(std::borrow::Borrow::borrow)
-            .collect();
+        let queries: BTreeSet<_> = response_schemas.keys().cloned().collect();
 
-        check_api_integrity::<Self>(&queries)?;
+        check_api_integrity::<Self>(queries)?;
 
         Ok(response_schemas)
     }
@@ -24,13 +21,13 @@ pub trait QueryResponses: JsonSchema {
 
 /// `generated_queries` is expected to be a sorted slice here!
 fn check_api_integrity<T: QueryResponses + ?Sized>(
-    generated_queries: &[&str],
+    generated_queries: BTreeSet<String>,
 ) -> Result<(), IntegrityError> {
     let schema = crate::schema_for!(T);
 
     // something more readable below?
 
-    let mut schema_queries: Vec<_> = schema
+    let schema_queries: BTreeSet<_> = schema
         .schema
         .subschemas
         .ok_or(IntegrityError::InvalidQueryMsgSchema)?
@@ -47,18 +44,17 @@ fn check_api_integrity<T: QueryResponses + ?Sized>(
                 .ok_or(IntegrityError::InvalidQueryMsgSchema)
         })
         .collect::<Result<_, _>>()?;
-    schema_queries.sort();
     if schema_queries != generated_queries {
         return Err(IntegrityError::InconsistentQueries {
             query_msg: schema_queries,
-            responses: generated_queries.iter().map(ToString::to_string).collect(),
+            responses: generated_queries,
         });
     }
 
     Ok(())
 }
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, PartialEq)]
 pub enum IntegrityError {
     #[error("the structure of the QueryMsg schema was unexpected")]
     InvalidQueryMsgSchema,
@@ -66,7 +62,61 @@ pub enum IntegrityError {
         "inconsistent queries - QueryMsg schema has {query_msg:?}, but query responses have {responses:?}"
     )]
     InconsistentQueries {
-        query_msg: Vec<String>,
-        responses: Vec<String>,
+        query_msg: BTreeSet<String>,
+        responses: BTreeSet<String>,
     },
+}
+
+#[cfg(test)]
+mod tests {
+    use schemars::schema_for;
+
+    use super::*;
+
+    #[derive(Debug, JsonSchema)]
+    #[serde(rename_all = "snake_case")]
+    #[allow(dead_code)]
+    pub enum GoodMsg {
+        BalanceFor { account: String },
+    }
+
+    impl QueryResponses for GoodMsg {
+        fn response_schemas_impl() -> BTreeMap<String, RootSchema> {
+            BTreeMap::from([("balance_for".to_string(), schema_for!(u128))])
+        }
+    }
+
+    #[test]
+    fn good_msg_works() {
+        let response_schemas = GoodMsg::response_schemas().unwrap();
+        assert_eq!(
+            response_schemas,
+            BTreeMap::from([("balance_for".to_string(), schema_for!(u128))])
+        );
+    }
+
+    #[derive(Debug, JsonSchema)]
+    #[serde(rename_all = "kebab-case")]
+    #[allow(dead_code)]
+    pub enum BadMsg {
+        BalanceFor { account: String },
+    }
+
+    impl QueryResponses for BadMsg {
+        fn response_schemas_impl() -> BTreeMap<String, RootSchema> {
+            BTreeMap::from([("balance_for".to_string(), schema_for!(u128))])
+        }
+    }
+
+    #[test]
+    fn bad_msg_fails() {
+        let err = BadMsg::response_schemas().unwrap_err();
+        assert_eq!(
+            err,
+            IntegrityError::InconsistentQueries {
+                query_msg: BTreeSet::from(["balance-for".to_string()]),
+                responses: BTreeSet::from(["balance_for".to_string()])
+            }
+        );
+    }
 }
