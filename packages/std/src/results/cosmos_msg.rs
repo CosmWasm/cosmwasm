97c9feddc3087a15242c7d42e6f341c771c72e5d
Assaf Morami <assaf.morami@gmail.com>
2022-07-06 15:39:14 +0300

    Revert previous commit for WasmMsg
    It's only needed in Query as it's the same as v0.10.
    WasmMsg was changed in v1 so we already have special case for it in SGX.

--- packages/std/src/results/cosmos_msg.rs
+++ packages/std/src/results/cosmos_msg.rs
@@ -125,7 +125,6 @@ pub enum WasmMsg {
         contract_addr: String,
         /// code_hash is the hex encoded hash of the code. This is used by Secret Network to harden against replaying the contract
         /// It is used to bind the request to a destination contract in a stronger way than just the contract address which can be faked
-        #[serde(rename = "callback_code_hash")]
         code_hash: String,
         /// msg is the json-encoded ExecuteMsg struct (as raw Binary)
         msg: Binary,
@@ -139,7 +138,6 @@ pub enum WasmMsg {
         code_id: u64,
         /// code_hash is the hex encoded hash of the code. This is used by Secret Network to harden against replaying the contract
         /// It is used to bind the request to a destination contract in a stronger way than just the contract address which can be faked
-        #[serde(rename = "callback_code_hash")]
         code_hash: String,
         /// msg is the JSON-encoded InstantiateMsg struct (as raw Binary)
         msg: Binary,
