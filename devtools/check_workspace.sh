a88b281bc3548e0af8e01d41fbd1e40f30175dad
Tomasz Kurcz <uint@lavabit.com>
2022-08-22 15:30:19 +0200

    rename `cw-check-contract` folder to `check`

--- devtools/check_workspace.sh
+++ devtools/check_workspace.sh
@@ -19,3 +19,4 @@ cargo fmt
 (cd packages/schema && cargo build && cargo clippy --all-targets -- -D warnings)
 (cd packages/schema-derive && cargo build && cargo clippy --all-targets -- -D warnings)
 (cd packages/vm && cargo build --features iterator,stargate && cargo clippy --all-targets --features iterator,stargate -- -D warnings)
+(cd packages/check && cargo build && cargo clippy --all-targets -- -D warnings)
---
+++ packages/check/README.md
@@ -0,0 +1,11 @@
+# cosmwasm-check
+
+It allows checking if the Wasm binary is a proper smart contract that's ready to be uploaded
+to the blockchain.
+
+Installation:
+
+``` sh
+cargo install cosmwasm-check
+```
+
