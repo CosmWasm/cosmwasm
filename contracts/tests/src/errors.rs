185d57bda30b591fec0283fe17dcc7d9f083713a
Tomasz Kurcz <uint@lavabit.com>
2022-08-29 19:01:49 +0200

    testing contract: remove unused Error variant
    Co-authored-by: Simon Warta <2603011+webmaster128@users.noreply.github.com>

--- contracts/tests/src/errors.rs
+++ contracts/tests/src/errors.rs
@@ -6,7 +6,4 @@ pub enum HackError {
     #[error("{0}")]
     /// this is needed so we can use `bucket.load(...)?` and have it auto-converted to the custom error
     Std(#[from] StdError),
-    // this is whatever we want
-    #[error("Unauthorized")]
-    Unauthorized {},
 }
