ce5da3800e522e0abf1240314bcbfb284bd0a9b2
Tomasz Kurcz <uint@lavabit.com>
2022-08-29 19:04:13 +0200

    Update contract schemas

--- contracts/tests/examples/schema.rs
+++ contracts/tests/examples/schema.rs
@@ -1,12 +1,12 @@
 use cosmwasm_schema::write_api;
 use cosmwasm_std::Empty;
 
-use tests::msg::ExecuteMsg;
+use tests::msg::{ExecuteMsg, QueryMsg};
 
 fn main() {
     write_api! {
         instantiate: Empty,
-        // query: QueryMsg,
+        query: QueryMsg,
         execute: ExecuteMsg,
     }
 }
