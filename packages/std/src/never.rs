a570cc417d6d073134a23c12b1edc05f2220b718
Simon Warta <simon@warta.it>
2022-11-24 10:22:46 +0100

    Improve Never documentation

--- packages/std/src/never.rs
+++ packages/std/src/never.rs
@@ -2,10 +2,14 @@
 /// where we want to ensure that no error is returned, such as
 /// the `ibc_packet_receive` entry point.
 ///
+/// In contrast to `Empty`, this does not have a JSON schema
+/// and cannot be used for message and query types.
+///
 /// Once the ! type is stable, this is not needed anymore.
 /// See <https://github.com/rust-lang/rust/issues/35121>.
 pub enum Never {}
 
+// The Debug implementation is needed to allow the use of `Result::unwrap`.
 impl core::fmt::Debug for Never {
     fn fmt(&self, _f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         // This is unreachable because no instance of Never can exist
@@ -13,6 +17,8 @@ impl core::fmt::Debug for Never {
     }
 }
 
+// The Display implementation is needed to fulfill the ToString requirement of
+// entry point errors: `Result<IbcReceiveResponse<C>, E>` with `E: ToString`.
 impl core::fmt::Display for Never {
     fn fmt(&self, _f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         // This is unreachable because no instance of Never can exist
