a4a6fbfc72454af32f77c7735e7e077b4d25083c
Simon Warta <simon@warta.it>
2023-01-04 23:13:42 +0100

    Split payment and cleanup implementations

--- contracts/burner/src/msg.rs
+++ contracts/burner/src/msg.rs
@@ -2,9 +2,22 @@ use cosmwasm_schema::cw_serde;
 
 #[cw_serde]
 pub struct MigrateMsg {
+    /// The address we send all remaining balance to
     pub payout: String,
 }
 
 /// A placeholder where we don't take any input
 #[cw_serde]
 pub struct InstantiateMsg {}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    /// Cleans up the given number of state elements.
+    /// Call this multiple times to increamentally clean up state.
+    Cleanup {
+        /// The number of state elements to delete.
+        ///
+        /// Set this to None for unlimited cleanup (if your state is small or you are feeling YOLO)
+        limit: Option<u32>,
+    },
+}
--- contracts/burner/tests/integration.rs
+++ contracts/burner/tests/integration.rs
@@ -17,16 +17,27 @@
 //!      });
 //! 4. Anywhere you see query(&deps, ...) you must replace it with query(&mut deps, ...)
 
-use cosmwasm_std::{coins, BankMsg, ContractResult, Order, Response, SubMsg};
-use cosmwasm_vm::testing::{instantiate, migrate, mock_env, mock_info, mock_instance};
+use cosmwasm_std::{coins, Attribute, BankMsg, ContractResult, Order, Response, SubMsg};
+use cosmwasm_vm::testing::{execute, instantiate, migrate, mock_env, mock_info, mock_instance};
 
-use burner::msg::{InstantiateMsg, MigrateMsg};
+use burner::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg};
 use cosmwasm_vm::Storage;
 
 // This line will test the output of cargo wasm
 static WASM: &[u8] = include_bytes!("../target/wasm32-unknown-unknown/release/burner.wasm");
 // You can uncomment this line instead to test productionified build from rust-optimizer
 // static WASM: &[u8] = include_bytes!("../contract.wasm");
+
+/// Gets the value of the first attribute with the given key
+fn first_attr(data: impl AsRef<[Attribute]>, search_key: &str) -> Option<String> {
+    data.as_ref().iter().find_map(|a| {
+        if a.key == search_key {
+            Some(a.value.clone())
+        } else {
+            None
+        }
+    })
+}
 
 #[test]
 fn instantiate_fails() {
@@ -44,20 +55,8 @@ fn instantiate_fails() {
 }
 
 #[test]
-fn migrate_cleans_up_data() {
+fn migrate_sends_funds() {
     let mut deps = mock_instance(WASM, &coins(123456, "gold"));
-
-    // store some sample data
-    deps.with_storage(|storage| {
-        storage.set(b"foo", b"bar").0.unwrap();
-        storage.set(b"key2", b"data2").0.unwrap();
-        storage.set(b"key3", b"cool stuff").0.unwrap();
-        let iter_id = storage.scan(None, None, Order::Ascending).0.unwrap();
-        let cnt = storage.all(iter_id).0.unwrap().len();
-        assert_eq!(3, cnt);
-        Ok(())
-    })
-    .unwrap();
 
     // change the verifier via migrate
     let payout = String::from("someone else");
@@ -75,12 +74,61 @@ fn migrate_cleans_up_data() {
             amount: coins(123456, "gold"),
         }),
     );
+}
+
+#[test]
+fn execute_cleans_up_data() {
+    let mut deps = mock_instance(WASM, &coins(123456, "gold"));
+
+    // store some sample data
+    deps.with_storage(|storage| {
+        storage.set(b"foo", b"bar").0.unwrap();
+        storage.set(b"key2", b"data2").0.unwrap();
+        storage.set(b"key3", b"cool stuff").0.unwrap();
+        let iter_id = storage.scan(None, None, Order::Ascending).0.unwrap();
+        let cnt = storage.all(iter_id).0.unwrap().len();
+        assert_eq!(cnt, 3);
+        Ok(())
+    })
+    .unwrap();
+
+    // change the verifier via migrate
+    let payout = String::from("someone else");
+    let msg = MigrateMsg { payout };
+    let _res: Response = migrate(&mut deps, mock_env(), msg).unwrap();
+
+    let res: Response = execute(
+        &mut deps,
+        mock_env(),
+        mock_info("anon", &[]),
+        ExecuteMsg::Cleanup { limit: Some(2) },
+    )
+    .unwrap();
+    assert_eq!(first_attr(res.attributes, "deleted_entries").unwrap(), "2");
+
+    // One item should be left
+    deps.with_storage(|storage| {
+        let iter_id = storage.scan(None, None, Order::Ascending).0.unwrap();
+        let cnt = storage.all(iter_id).0.unwrap().len();
+        assert_eq!(cnt, 1);
+        Ok(())
+    })
+    .unwrap();
+
+    let res: Response = execute(
+        &mut deps,
+        mock_env(),
+        mock_info("anon", &[]),
+        ExecuteMsg::Cleanup { limit: Some(2) },
+    )
+    .unwrap();
+    assert_eq!(first_attr(res.attributes, "deleted_entries").unwrap(), "1");
 
     // check there is no data in storage
     deps.with_storage(|storage| {
         let iter_id = storage.scan(None, None, Order::Ascending).0.unwrap();
         let cnt = storage.all(iter_id).0.unwrap().len();
-        assert_eq!(0, cnt);
+        assert_eq!(cnt, 0);
         Ok(())
     })
     .unwrap();
