a529b7624389e7601b490ecbb3609d026ed26180
Simon Warta <simon@warta.it>
2023-03-02 12:15:37 +0100

    Remove deny_unknown_fields

--- packages/schema-derive/src/cw_serde.rs
+++ packages/schema-derive/src/cw_serde.rs
@@ -12,7 +12,6 @@ pub fn cw_serde_impl(input: DeriveInput) -> DeriveInput {
                 ::cosmwasm_schema::schemars::JsonSchema
             )]
             #[allow(clippy::derive_partial_eq_without_eq)] // Allow users of `#[cw_serde]` to not implement Eq without clippy complaining
-            #[serde(deny_unknown_fields, crate = "::cosmwasm_schema::serde")]
             #[schemars(crate = "::cosmwasm_schema::schemars")]
             #input
         },
@@ -26,7 +25,7 @@ pub fn cw_serde_impl(input: DeriveInput) -> DeriveInput {
                 ::cosmwasm_schema::schemars::JsonSchema
             )]
             #[allow(clippy::derive_partial_eq_without_eq)] // Allow users of `#[cw_serde]` to not implement Eq without clippy complaining
-            #[serde(deny_unknown_fields, rename_all = "snake_case", crate = "::cosmwasm_schema::serde")]
+            #[serde(rename_all = "snake_case", crate = "::cosmwasm_schema::serde")]
             #[schemars(crate = "::cosmwasm_schema::schemars")]
             #input
         },
@@ -57,7 +56,6 @@ mod tests {
                 ::cosmwasm_schema::schemars::JsonSchema
             )]
             #[allow(clippy::derive_partial_eq_without_eq)]
-            #[serde(deny_unknown_fields, crate = "::cosmwasm_schema::serde")]
             #[schemars(crate = "::cosmwasm_schema::schemars")]
             pub struct InstantiateMsg {
                 pub verifier: String,
@@ -84,7 +82,6 @@ mod tests {
                 ::cosmwasm_schema::schemars::JsonSchema
             )]
             #[allow(clippy::derive_partial_eq_without_eq)]
-            #[serde(deny_unknown_fields, crate = "::cosmwasm_schema::serde")]
             #[schemars(crate = "::cosmwasm_schema::schemars")]
             pub struct InstantiateMsg {}
         };
@@ -113,7 +110,7 @@ mod tests {
                 ::cosmwasm_schema::schemars::JsonSchema
             )]
             #[allow(clippy::derive_partial_eq_without_eq)]
-            #[serde(deny_unknown_fields, rename_all = "snake_case", crate = "::cosmwasm_schema::serde")]
+            #[serde(rename_all = "snake_case", crate = "::cosmwasm_schema::serde")]
             #[schemars(crate = "::cosmwasm_schema::schemars")]
             pub enum SudoMsg {
                 StealFunds {
