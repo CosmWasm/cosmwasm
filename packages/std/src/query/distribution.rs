8d91dd2235b8cdd0c4a966680d3e206ce927768f
Christoph Otter <chris@confio.gmbh>
2023-06-23 14:48:57 +0200

    Implement QueryResponseType for DelegatorWithdrawAddressResponse

--- packages/std/src/query/distribution.rs
+++ packages/std/src/query/distribution.rs
@@ -1,6 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use super::query_response::QueryResponseType;
+
 #[non_exhaustive]
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
@@ -10,8 +12,11 @@ pub enum DistributionQuery {
 }
 
 // https://github.com/cosmos/cosmos-sdk/blob/4f6f6c00021f4b5ee486bbb71ae2071a8ceb47c9/x/distribution/types/query.pb.go#L832-L835
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
+#[non_exhaustive]
 pub struct DelegatorWithdrawAddressResponse {
     pub withdraw_address: String,
 }
+
+impl QueryResponseType for DelegatorWithdrawAddressResponse {}
