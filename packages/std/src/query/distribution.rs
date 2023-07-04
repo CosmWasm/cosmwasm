580277dd0daf0762f9f3c91570f28f79e74e118a
Christoph Otter <chris@confio.gmbh>
2023-07-04 13:38:26 +0200

    Use Addr in DelegatorWithdrawAddressResponse

--- packages/std/src/query/distribution.rs
+++ packages/std/src/query/distribution.rs
@@ -1,7 +1,7 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use super::query_response::QueryResponseType;
+use crate::Addr;
 
 #[non_exhaustive]
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
@@ -12,11 +12,23 @@ pub enum DistributionQuery {
 }
 
 // https://github.com/cosmos/cosmos-sdk/blob/4f6f6c00021f4b5ee486bbb71ae2071a8ceb47c9/x/distribution/types/query.pb.go#L832-L835
-#[derive(Serialize, Deserialize, Clone, Debug, Default, PartialEq, Eq, JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 #[non_exhaustive]
 pub struct DelegatorWithdrawAddressResponse {
-    pub withdraw_address: String,
+    pub withdraw_address: Addr,
 }
 
-impl QueryResponseType for DelegatorWithdrawAddressResponse {}
+impl DelegatorWithdrawAddressResponse {
+    /// Constructor for testing frameworks such as cw-multi-test.
+    /// This is required because query response types should be #[non_exhaustive].
+    /// As a contract developer you should not need this constructor since
+    /// query responses are constructed for you via deserialization.
+    ///
+    /// Warning: This is for cw-multi-test use only and can change at any time.
+    #[doc(hidden)]
+    #[allow(dead_code)]
+    pub fn from_parts(withdraw_address: Addr) -> Self {
+        Self { withdraw_address }
+    }
+}
