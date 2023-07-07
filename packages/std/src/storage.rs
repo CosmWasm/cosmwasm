11461e4ee290f158ac570a3064e67b4e0f838fe5
Ethan Frey <ethanfrey@users.noreply.github.com>
2023-07-08 01:03:28 +0200

    Allow writing empty bytes, as prost often does

--- packages/std/src/storage.rs
+++ packages/std/src/storage.rs
@@ -26,9 +26,10 @@ impl Storage for MemoryStorage {
     }
 
     fn set(&mut self, key: &[u8], value: &[u8]) {
-        if value.is_empty() {
-            panic!("TL;DR: Value must not be empty in Storage::set but in most cases you can use Storage::remove instead. Long story: Getting empty values from storage is not well supported at the moment. Some of our internal interfaces cannot differentiate between a non-existent key and an empty value. Right now, you cannot rely on the behaviour of empty values. To protect you from trouble later on, we stop here. Sorry for the inconvenience! We highly welcome you to contribute to CosmWasm, making this more solid one way or the other.");
-        }
+        // TODO: enable? Prost often writes empty keys for eg 0
+        // if value.is_empty() {
+        //     panic!("TL;DR: Value must not be empty in Storage::set but in most cases you can use Storage::remove instead. Long story: Getting empty values from storage is not well supported at the moment. Some of our internal interfaces cannot differentiate between a non-existent key and an empty value. Right now, you cannot rely on the behaviour of empty values. To protect you from trouble later on, we stop here. Sorry for the inconvenience! We highly welcome you to contribute to CosmWasm, making this more solid one way or the other.");
+        // }
 
         self.data.insert(key.to_vec(), value.to_vec());
     }
