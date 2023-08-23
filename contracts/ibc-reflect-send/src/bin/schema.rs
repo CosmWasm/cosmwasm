f28adbe8952e4688e5fe4ef894681fdd41eac1c8
Simon Warta <simon@warta.it>
2023-08-23 09:59:22 +0200

    Add AcknowledgementMsgs to ibc-reflect-send schemas

--- contracts/ibc-reflect-send/src/bin/schema.rs
+++ contracts/ibc-reflect-send/src/bin/schema.rs
@@ -1,8 +1,10 @@
 use std::env::current_dir;
 
-use cosmwasm_schema::{export_schema, schema_for, write_api};
+use cosmwasm_schema::{export_schema, export_schema_with_title, schema_for, write_api};
 
-use ibc_reflect_send::ibc_msg::PacketMsg;
+use ibc_reflect_send::ibc_msg::{
+    AcknowledgementMsg, BalancesResponse, DispatchResponse, PacketMsg, WhoAmIResponse,
+};
 use ibc_reflect_send::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
@@ -17,4 +19,19 @@ fn main() {
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema");
     export_schema(&schema_for!(PacketMsg), &out_dir);
+    export_schema_with_title(
+        &schema_for!(AcknowledgementMsg<BalancesResponse>),
+        &out_dir,
+        "AcknowledgementMsgBalances",
+    );
+    export_schema_with_title(
+        &schema_for!(AcknowledgementMsg<DispatchResponse>),
+        &out_dir,
+        "AcknowledgementMsgDispatch",
+    );
+    export_schema_with_title(
+        &schema_for!(AcknowledgementMsg<WhoAmIResponse>),
+        &out_dir,
+        "AcknowledgementMsgWhoAmI",
+    );
 }
