ca4fc371084cea483cb47aa167ea4d362f75fed9
llllllluc <58892938+llllllluc@users.noreply.github.com>
2023-09-17 00:07:21 -0700

    nit

--- packages/crypto/src/secp256k1.rs
+++ packages/crypto/src/secp256k1.rs
@@ -44,10 +44,7 @@ pub fn secp256k1_verify(
     public_key: &[u8],
 ) -> CryptoResult<bool> {
     let message_hash = read_hash(message_hash)?;
-    let signature = read_signature(signature)?;
-    // panic!("{}", signature);
-    println!("Valid signature: {:?}", signature);
-
+    let signature = read_signature(signature)?;signature);
     check_pubkey(public_key)?;
 
     // Already hashed, just build Digest container
