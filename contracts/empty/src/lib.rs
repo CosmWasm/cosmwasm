cf3fc5734acd921ac6e61c1bae360a26f542a390
Christoph Otter <chris@confio.gmbh>
2023-10-31 10:14:34 +0100

    Change wording

--- contracts/empty/src/lib.rs
+++ contracts/empty/src/lib.rs
@@ -1,2 +1,2 @@
-// Make sure to reexport cosmwasm_std, so it is kept in the final wasm
+// Make sure to reexport cosmwasm_std, so the standard entry points are kept in the final wasm
 pub use cosmwasm_std;
