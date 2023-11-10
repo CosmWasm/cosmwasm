a2ca7ba6345f7310daab74e15c207e98e7b7b5cc
Christoph Otter <chris@confio.gmbh>
2023-11-10 15:04:30 +0100

    Add Checksum::from_hex

--- packages/std/src/checksum.rs
+++ packages/std/src/checksum.rs
@@ -3,6 +3,8 @@ use core::fmt;
 use sha2::{Digest, Sha256};
 use thiserror::Error;
 
+use crate::{StdError, StdResult};
+
 /// A SHA-256 checksum of a Wasm blob, used to identify a Wasm code.
 /// This must remain stable since this checksum is stored in the blockchain state.
 ///
@@ -14,6 +16,15 @@ pub struct Checksum([u8; 32]);
 impl Checksum {
     pub fn generate(wasm: &[u8]) -> Self {
         Checksum(Sha256::digest(wasm).into())
+    }
+
+    /// Tries to parse the given hex string into a checksum.
+    /// Errors if the string contains non-hex characters or does not contain 32 bytes.
+    pub fn from_hex(input: &str) -> StdResult<Self> {
+        let mut binary = [0u8; 32];
+        hex::decode_to_slice(input, &mut binary).map_err(StdError::invalid_hex)?;
+
+        Ok(Self(binary))
     }
 
     /// Creates a lowercase hex encoded copy of this checksum.
@@ -94,6 +105,24 @@ mod tests {
             checksum.to_string(),
             "722c8c993fd75a7627d69ed941344fe2a1423a3e75efd3e6778a142884227104"
         );
+    }
+
+    #[test]
+    fn from_hex_works() {
+        // echo -n "hij" | sha256sum
+        let checksum = "722c8c993fd75a7627d69ed941344fe2a1423a3e75efd3e6778a142884227104";
+        let parsed = Checksum::from_hex(checksum).unwrap();
+        assert_eq!(parsed, Checksum::generate(b"hij"));
+        // should be inverse of `to_hex`
+        assert_eq!(parsed.to_hex(), checksum);
+
+        // invalid hex
+        let too_short = "722c8c993fd75a7627d69ed941344fe2a1423a3e75efd3e6778a1428842271";
+        assert!(Checksum::from_hex(too_short).is_err());
+        let invalid_char = "722c8c993fd75a7627d69ed941344fe2a1423a3e75efd3e6778a1428842271g4";
+        assert!(Checksum::from_hex(invalid_char).is_err());
+        let too_long = "722c8c993fd75a7627d69ed941344fe2a1423a3e75efd3e6778a14288422710400";
+        assert!(Checksum::from_hex(too_long).is_err());
     }
 
     #[test]
