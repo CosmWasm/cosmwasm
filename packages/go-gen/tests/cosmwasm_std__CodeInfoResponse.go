fac1e25ee630424d249fe66e8d504aedb1df8978
Christoph Otter <chris@confio.gmbh>
2023-11-20 11:22:14 +0100

    Fix CodeInfoResponse comment
    Co-authored-by: Simon Warta <2603011+webmaster128@users.noreply.github.com>

--- packages/go-gen/tests/cosmwasm_std__CodeInfoResponse.go
+++ packages/go-gen/tests/cosmwasm_std__CodeInfoResponse.go
@@ -1,5 +1,5 @@
 type CodeInfoResponse struct {
-	Checksum Checksum `json:"checksum"` // in wasmvm, this is `omitempty`
+	Checksum Checksum `json:"checksum"` // before wasmvm 2.0.0, this was `omitempty` (https://github.com/CosmWasm/wasmvm/issues/471)
 	CodeID   uint64   `json:"code_id"`
 	Creator  string   `json:"creator"`
 }
