1f397bbc80cc2df100048034fa80f8c541220af6
Christoph Otter <chris@confio.gmbh>
2023-11-20 12:48:11 +0100

    Fix lint

--- packages/vm/src/static_analysis.rs
+++ packages/vm/src/static_analysis.rs
@@ -38,7 +38,7 @@ pub enum Entrypoint {
 // sort entrypoints by their &str representation
 impl PartialOrd for Entrypoint {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.as_ref().partial_cmp(other.as_ref())
+        Some(self.cmp(other))
     }
 }
 impl Ord for Entrypoint {
