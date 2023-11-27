c4fd8133db6cc155defa911a624f827cbc3d91bc
Christoph Otter <chris@confio.gmbh>
2023-11-27 17:34:30 +0000

    Always add backtrace to cosmwasm-std

---
+++ packages/std/src/errors/backtrace.rs
@@ -0,0 +1,48 @@
+use core::fmt::{Debug, Display, Formatter, Result};
+use thiserror::Error;
+
+/// This wraps an actual backtrace to achieve two things:
+/// - being able to fill this with a stub implementation in `no_std` environments
+/// - being able to use this in conjunction with [`thiserror::Error`]
+#[derive(Error)]
+pub struct BT(Box<dyn Printable>);
+
+impl BT {
+    #[track_caller]
+    pub fn capture() -> Self {
+        BT(Box::new(std::backtrace::Backtrace::capture()))
+    }
+}
+
+trait Printable: Debug + Display {}
+impl<T> Printable for T where T: Debug + Display {}
+
+impl Debug for BT {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Debug::fmt(&self.0, f)
+    }
+}
+
+impl Display for BT {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        Display::fmt(&self.0, f)
+    }
+}
+
+/// This macro implements `From` for a given error type to a given error type where
+/// the target error has a `backtrace` field.
+/// This is meant as a replacement for `thiserror`'s `#[from]` attribute, which does not
+/// work with our custom backtrace wrapper.
+macro_rules! impl_from_err {
+    ($from:ty, $to:ty, $map:path) => {
+        impl From<$from> for $to {
+            fn from(err: $from) -> Self {
+                $map {
+                    source: err,
+                    backtrace: $crate::errors::backtrace::BT::capture(),
+                }
+            }
+        }
+    };
+}
+pub(crate) use impl_from_err;
