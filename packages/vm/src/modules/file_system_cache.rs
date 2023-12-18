8e205e658b209b1152f75781414144da5e676ba0
Christoph Otter <chris@confio.gmbh>
2023-12-18 18:12:00 +0100

    Catch panic

--- packages/vm/src/modules/file_system_cache.rs
+++ packages/vm/src/modules/file_system_cache.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::hash::Hash;
 use std::io;
+use std::panic::catch_unwind;
 use std::path::{Path, PathBuf};
 use thiserror::Error;
 
@@ -157,9 +158,12 @@ impl FileSystemCache {
 
         let filename = checksum.to_hex();
         let path = self.modules_path.join(filename);
-        module
-            .serialize_to_file(&path)
-            .map_err(|e| VmError::cache_err(format!("Error writing module to disk: {e}")))?;
+        catch_unwind(|| {
+            module
+                .serialize_to_file(&path)
+                .map_err(|e| VmError::cache_err(format!("Error writing module to disk: {e}")))
+        })
+        .map_err(|_| VmError::cache_err("Could not write module to disk"))??;
         let module_size = module_size(&path)?;
         Ok(module_size)
     }
