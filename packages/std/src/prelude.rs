fb01967235f79b98d0245484a09d37bf67107f0a
Christoph Otter <chris@confio.gmbh>
2023-12-20 18:32:00 +0100

    Fix builds

--- packages/std/src/prelude.rs
+++ packages/std/src/prelude.rs
@@ -4,6 +4,3 @@ pub use alloc::string::{String, ToString};
 pub use alloc::vec;
 pub use alloc::vec::Vec;
 pub use core::option::Option::{self, None, Some};
-
-#[cfg(not(feature = "std"))]
-core::compile_error!("Please enable `cosmwasm-std`'s `std` feature, as we might move existing functionality to that feature in the future.");
