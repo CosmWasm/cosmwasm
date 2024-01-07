f34226fdd83b553d2c16b82eeb2ccb3e6f9d2c24
Mauro Lacy <mauro@lacy.com.es>
2024-01-07 19:23:57 +0100

    Add a couple more test vectors

--- packages/crypto/src/secp256r1.rs
+++ packages/crypto/src/secp256r1.rs
@@ -118,21 +118,22 @@ mod tests {
     // For generic signature verification
     const MSG: &str = "Hello World!";
 
-    // Cosmos secp256r1 signature verification
-    // tendermint/PubKeySecp256r1 pubkey
-    // ECDSA/P-256 test vectors adapted from the FIPS 186-4 ECDSA test vectors.
-    // (P-256, SHA-256, from `SigGen.txt` in `186-4ecdsatestvectors.zip`)
+    // Cosmos secp256r1 signature verification. Matches tendermint/PubKeySecp256r1 pubkey format.
+    // ECDSA/P-256 test vectors adapted from the FIPS 186-4 ECDSA test vectors (P-256, SHA-256, from
+    // `SigGen.txt` in `186-4ecdsatestvectors.zip`).
     // <https://csrc.nist.gov/projects/cryptographic-algorithm-validation-program/digital-signatures>
-    const COSMOS_SECP256R1_PUBKEY_HEX: &str =
+    const COSMOS_SECP256R1_PUBKEY_HEX1: &str =
         "041ccbe91c075fc7f4f033bfa248db8fccd3565de94bbfb12f3c59ff46c271bf83ce4014c68811f9a21a1fdb2c0e6113e06db7ca93b7404e78dc7ccd5ca89a4ca9";
+    const COSMOS_SECP256R1_PUBKEY_HEX2: &str = "04e266ddfdc12668db30d4ca3e8f7749432c416044f2d2b8c10bf3d4012aeffa8abfa86404a2e9ffe67d47c587ef7a97a7f456b863b4d02cfc6928973ab5b1cb39";
+    const COSMOS_SECP256R1_PUBKEY_HEX3: &str = "0474ccd8a62fba0e667c50929a53f78c21b8ff0c3c737b0b40b1750b2302b0bde829074e21f3a0ef88b9efdf10d06aa4c295cc1671f758ca0e4cd108803d0f2614";
 
     const COSMOS_SECP256R1_MSG_HEX1: &str = "5905238877c77421f73e43ee3da6f2d9e2ccad5fc942dcec0cbd25482935faaf416983fe165b1a045ee2bcd2e6dca3bdf46c4310a7461f9a37960ca672d3feb5473e253605fb1ddfd28065b53cb5858a8ad28175bf9bd386a5e471ea7a65c17cc934a9d791e91491eb3754d03799790fe2d308d16146d5c9b0d0debd97d79ce8";
-    // const COSMOS_SECP256R1_MSG_HEX2: &str = "";
-    // const COSMOS_SECP256R1_MSG_HEX3: &str = "";
+    const COSMOS_SECP256R1_MSG_HEX2: &str = "c35e2f092553c55772926bdbe87c9796827d17024dbb9233a545366e2e5987dd344deb72df987144b8c6c43bc41b654b94cc856e16b96d7a821c8ec039b503e3d86728c494a967d83011a0e090b5d54cd47f4e366c0912bc808fbb2ea96efac88fb3ebec9342738e225f7c7c2b011ce375b56621a20642b4d36e060db4524af1";
+    const COSMOS_SECP256R1_MSG_HEX3: &str = "3c054e333a94259c36af09ab5b4ff9beb3492f8d5b4282d16801daccb29f70fe61a0b37ffef5c04cd1b70e85b1f549a1c4dc672985e50f43ea037efa9964f096b5f62f7ffdf8d6bfb2cc859558f5a393cb949dbd48f269343b5263dcdb9c556eca074f2e98e6d94c2c29a677afaf806edf79b15a3fcd46e7067b7669f83188ee";
 
     const COSMOS_SECP256R1_SIGNATURE_HEX1: &str = "f3ac8061b514795b8843e3d6629527ed2afd6b1f6a555a7acabb5e6f79c8c2ac8bf77819ca05a6b2786c76262bf7371cef97b218e96f175a3ccdda2acc058903";
-    // const COSMOS_SECP256R1_SIGNATURE_HEX2: &str = "";
-    // const COSMOS_SECP256R1_SIGNATURE_HEX3: &str = "";
+    const COSMOS_SECP256R1_SIGNATURE_HEX2: &str = "976d3a4e9d23326dc0baa9fa560b7c4e53f42864f508483a6473b6a11079b2db1b766e9ceb71ba6c01dcd46e0af462cd4cfa652ae5017d4555b8eeefe36e1932";
+    const COSMOS_SECP256R1_SIGNATURE_HEX3: &str = "35fb60f5ca0f3ca08542fb3cc641c8263a2cab7a90ee6a5e1583fac2bb6f6bd1ee59d81bc9db1055cc0ed97b159d8784af04e98511d0a9a407b99bb292572e96";
 
     // Test data originally from https://github.com/cosmos/cosmjs/blob/v0.24.0-alpha.22/packages/crypto/src/secp256k1.spec.ts#L195-L394
     const COSMOS_SECP256R1_TESTS_JSON: &str = "./testdata/secp256r1_tests.json";
@@ -192,20 +193,24 @@ mod tests {
 
     #[test]
     fn test_cosmos_secp256r1_verify() {
-        let public_key = hex::decode(COSMOS_SECP256R1_PUBKEY_HEX).unwrap();
-
-        for ((i, msg), sig) in (1..)
+        for (((i, pk), msg), sig) in (1..)
+            .zip(&[
+                COSMOS_SECP256R1_PUBKEY_HEX1,
+                COSMOS_SECP256R1_PUBKEY_HEX2,
+                COSMOS_SECP256R1_PUBKEY_HEX3,
+             ])
             .zip(&[
                 COSMOS_SECP256R1_MSG_HEX1,
-                //COSMOS_SECP256R1_MSG_HEX2,
-                //COSMOS_SECP256R1_MSG_HEX3,
+                COSMOS_SECP256R1_MSG_HEX2,
+                COSMOS_SECP256R1_MSG_HEX3,
             ])
             .zip(&[
                 COSMOS_SECP256R1_SIGNATURE_HEX1,
-                //COSMOS_SECP256R1_SIGNATURE_HEX2,
-                //COSMOS_SECP256R1_SIGNATURE_HEX3,
+                COSMOS_SECP256R1_SIGNATURE_HEX2,
+                COSMOS_SECP256R1_SIGNATURE_HEX3,
             ])
         {
+            let public_key = hex::decode(pk).unwrap();
             let message = hex::decode(msg).unwrap();
             let signature = hex::decode(sig).unwrap();
 
