0e947669997a8ef059964d9958b9ba0619fadb7c
Simon Warta <simon@warta.it>
2024-01-23 01:35:50 +0100

    Add wycheproof tests for ECDSA secp256k1

---
+++ packages/crypto/tests/wycheproof_secp256k1.rs
@@ -0,0 +1,288 @@
+#![allow(clippy::single_match)] // Only needed for old clippy (e.g. 1.70.0)
+
+use cosmwasm_crypto::secp256k1_verify;
+use serde::Deserialize;
+use sha2::{Digest, Sha256, Sha512};
+
+// In repo root
+// curl -sS -L https://github.com/google/wycheproof/raw/master/testvectors_v1/ecdsa_secp256k1_sha256_test.json > packages/crypto/testdata/wycheproof/ecdsa_secp256k1_sha256_test.json
+// curl -sS -L https://github.com/google/wycheproof/raw/master/testvectors_v1/ecdsa_secp256k1_sha512_test.json > packages/crypto/testdata/wycheproof/ecdsa_secp256k1_sha512_test.json
+
+const SECP256K1_SHA256: &str = "./testdata/wycheproof/ecdsa_secp256k1_sha256_test.json";
+const SECP256K1_SHA512: &str = "./testdata/wycheproof/ecdsa_secp256k1_sha512_test.json";
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct File {
+    number_of_tests: usize,
+    test_groups: Vec<TestGroup>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct TestGroup {
+    public_key: Key,
+    tests: Vec<TestCase>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Key {
+    uncompressed: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct TestCase {
+    tc_id: u32,
+    msg: String,
+    sig: String,
+    // "acceptable", "valid" or "invalid"
+    result: String,
+}
+
+fn read_file(path: &str) -> File {
+    use std::fs::File;
+    use std::io::BufReader;
+
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+
+    serde_json::from_reader(reader).unwrap()
+}
+
+#[test]
+fn test_ecdsa_secp256k1_sha256() {
+    let mut tested: usize = 0;
+    let File {
+        number_of_tests,
+        test_groups,
+    } = read_file(SECP256K1_SHA256);
+    assert_eq!(number_of_tests, 463);
+
+    for group in test_groups {
+        let public_key = hex::decode(group.public_key.uncompressed).unwrap();
+        assert_eq!(public_key.len(), 65);
+
+        for tc in group.tests {
+            tested += 1;
+            assert_eq!(tc.tc_id as usize, tested);
+            // eprintln!("Test case ID: {}", tc.tc_id);
+
+            match tc.result.as_str() {
+                "valid" | "acceptable" => {
+                    let message = hex::decode(tc.msg).unwrap();
+                    let message_hash: [u8; 32] = Sha256::digest(message).into();
+                    let der_signature = hex::decode(tc.sig).unwrap();
+                    let signature = from_der(&der_signature).unwrap();
+                    let valid = secp256k1_verify(&message_hash, &signature, &public_key).unwrap();
+                    assert!(valid);
+                }
+                "invalid" => {
+                    let message = hex::decode(tc.msg).unwrap();
+                    let message_hash = Sha256::digest(message);
+                    let der_signature = hex::decode(tc.sig).unwrap();
+
+                    if let Ok(signature) = from_der(&der_signature) {
+                        match secp256k1_verify(&message_hash, &signature, &public_key) {
+                            Ok(valid) => assert!(!valid),
+                            Err(_) => { /* this is expected for "invalid", all good */ }
+                        }
+                    } else {
+                        // invalid DER encoding, okay
+                    }
+                }
+                _ => panic!("Found unexpected result value"),
+            }
+            if tc.result == "valid" {}
+        }
+    }
+    assert_eq!(tested, number_of_tests);
+}
+
+#[test]
+fn test_ecdsa_secp256k1_sha512() {
+    let mut tested: usize = 0;
+    let File {
+        number_of_tests,
+        test_groups,
+    } = read_file(SECP256K1_SHA512);
+    assert_eq!(number_of_tests, 533);
+
+    for group in test_groups {
+        let public_key = hex::decode(group.public_key.uncompressed).unwrap();
+        assert_eq!(public_key.len(), 65);
+
+        for tc in group.tests {
+            tested += 1;
+            assert_eq!(tc.tc_id as usize, tested);
+            // eprintln!("Test case ID: {}", tc.tc_id);
+
+            match tc.result.as_str() {
+                "valid" | "acceptable" => {
+                    let message = hex::decode(tc.msg).unwrap();
+                    let message_hash = Sha512::digest(message);
+                    let der_signature = hex::decode(tc.sig).unwrap();
+                    let signature = from_der(&der_signature).unwrap();
+                    let valid = secp256k1_verify(&message_hash, &signature, &public_key).unwrap();
+                    assert!(valid);
+                }
+                "invalid" => {
+                    let message = hex::decode(tc.msg).unwrap();
+                    let message_hash = Sha512::digest(message);
+                    let der_signature = hex::decode(tc.sig).unwrap();
+
+                    if let Ok(signature) = from_der(&der_signature) {
+                        match secp256k1_verify(&message_hash, &signature, &public_key) {
+                            Ok(valid) => assert!(!valid),
+                            Err(_) => { /* this is expected for "invalid", all good */ }
+                        }
+                    } else {
+                        // invalid DER encoding, okay
+                    }
+                }
+                _ => panic!("Found unexpected result value"),
+            }
+            if tc.result == "valid" {}
+        }
+    }
+    assert_eq!(tested, number_of_tests);
+}
+
+fn from_der(data: &[u8]) -> Result<[u8; 64], &str> {
+    const DER_TAG_INTEGER: u8 = 0x02;
+
+    let mut pos = 0;
+
+    let Some(prefix) = data.get(pos) else {
+        return Err("Could not read prefix");
+    };
+    pos += 1;
+    if *prefix != 0x30 {
+        return Err("Prefix 0x30 expected");
+    }
+
+    let Some(body_length) = data.get(pos) else {
+        return Err("Could not read body length");
+    };
+    pos += 1;
+    if data.len() - pos != *body_length as usize {
+        return Err("Data length mismatch detected");
+    }
+
+    // r
+    let Some(r_tag) = data.get(pos) else {
+        return Err("Could not read r_tag");
+    };
+    pos += 1;
+    if *r_tag != DER_TAG_INTEGER {
+        return Err("INTEGER tag expected");
+    }
+    let Some(r_length) = data.get(pos).map(|rl: &u8| *rl as usize) else {
+        return Err("Could not read r_length");
+    };
+    pos += 1;
+    if r_length >= 0x80 {
+        return Err("Decoding length values above 127 not supported");
+    }
+    if pos + r_length > data.len() {
+        return Err("R lengths exceeds end of data");
+    }
+    let mut r_data = &data[pos..pos + r_length];
+    pos += r_length;
+
+    // s
+    let Some(s_tag) = data.get(pos) else {
+        return Err("Could not read s_tag");
+    };
+    pos += 1;
+    if *s_tag != DER_TAG_INTEGER {
+        return Err("INTEGER tag expected");
+    }
+    let Some(s_length) = data.get(pos).map(|sl| *sl as usize) else {
+        return Err("Could not read s_length");
+    };
+    pos += 1;
+    if s_length >= 0x80 {
+        return Err("Decoding length values above 127 not supported");
+    }
+    if pos + s_length > data.len() {
+        return Err("S lengths exceeds end of data");
+    }
+    let mut s_data = &data[pos..pos + s_length];
+    pos += s_length;
+
+    if pos != data.len() {
+        return Err("Extra bytes in data input");
+    }
+
+    if r_data.is_empty() {
+        return Err("r_data is empty");
+    }
+
+    if (r_data[0] & 0x80) != 0 {
+        return Err("r_data missing leading zero");
+    }
+
+    if r_data.len() > 1 && r_data[0] == 0 {
+        r_data = &r_data[1..];
+        if (r_data[0] & 0x80) == 0 {
+            return Err("r_data has invalid leading zero");
+        }
+    }
+
+    // if r_data.len() > 1 && r_data[0] == 0xff && r_data[1] & 0x80 != 0 {
+    //     return Err("r_data missing leading zero");
+    // }
+
+    if r_data.len() > 32 {
+        return Err("r_data exceeded 32 bytes");
+    }
+
+    if s_data.is_empty() {
+        return Err("s_data is empty");
+    }
+
+    if (s_data[0] & 0x80) != 0 {
+        return Err("s_data missing leading zero");
+    }
+
+    if s_data.len() > 1 && s_data[0] == 0 {
+        s_data = &s_data[1..];
+        if (s_data[0] & 0x80) == 0 {
+            return Err("s_data has invalid leading zero");
+        }
+    }
+
+    // if s_data.len() > 1 && s_data[0] == 0xff && s_data[1] & 0x80 != 0 {
+    //     return Err("s_data missing leading zero");
+    // }
+
+    if s_data.len() > 32 {
+        return Err("s_data exceeded 32 bytes");
+    }
+
+    let mut out = [0u8; 64];
+    // r/s data can contain leading 0 bytes to express integers being non-negative in DER
+    out[0..32].copy_from_slice(&pad_to_32(r_data));
+    out[32..].copy_from_slice(&pad_to_32(s_data));
+    Ok(out)
+}
+
+//fn trim_leading_null_bytes(input: &[u8]) -> &[u8] {
+//    let mut data = input;
+//    loop {
+//        match data.first() {
+//            Some(0x00) => data = &data[1..],
+//            Some(_) | None => return data,
+//        }
+//    }
+//}
+
+fn pad_to_32(input: &[u8]) -> [u8; 32] {
+    let shift = 32 - input.len();
+    let mut out = [0u8; 32];
+    out[shift..].copy_from_slice(input);
+    out
+}
