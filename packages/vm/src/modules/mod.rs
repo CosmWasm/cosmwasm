7f9669201c0eea41acd12161da4407941e661fdc
Mauro Lacy <mauro@lacy.com.es>
2024-01-23 10:46:52 +0100

    Fix rebase errors

--- packages/vm/src/modules/mod.rs
+++ packages/vm/src/modules/mod.rs
@@ -5,7 +5,7 @@ mod pinned_memory_cache;
 mod versioning;
 
 pub use cached_module::CachedModule;
-pub use file_system_cache::{FileSystemCache, NewFileSystemCacheError};
+pub use file_system_cache::FileSystemCache;
 pub use in_memory_cache::InMemoryCache;
 pub use pinned_memory_cache::PinnedMemoryCache;
 pub use versioning::current_wasmer_module_version;
