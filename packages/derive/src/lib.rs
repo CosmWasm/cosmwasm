b2bcbcbae6afa0ecf7c93cd59d95f90f10890592
Aumetra Weisman <aumetra@cryptolab.net>
2024-03-22 16:12:53 +0100

    Make crate used in `entry_point` configurable

--- packages/derive/src/lib.rs
+++ packages/derive/src/lib.rs
@@ -1,6 +1,45 @@
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::parse_macro_input;
+use quote::{format_ident, quote, ToTokens};
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input, parse_quote,
+    punctuated::Punctuated,
+    Token,
+};
+
+struct Options {
+    crate_path: syn::Path,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            crate_path: parse_quote!(::cosmwasm_std),
+        }
+    }
+}
+
+impl Parse for Options {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut ret = Self::default();
+        if input.is_empty() {
+            return Ok(ret);
+        }
+
+        let attrs = Punctuated::<syn::MetaNameValue, Token![,]>::parse_terminated(input)?;
+
+        for kv in attrs {
+            if kv.path.is_ident("crate") {
+                let path_as_string: syn::LitStr = syn::parse2(kv.value.to_token_stream())?;
+                ret.crate_path = path_as_string.parse()?
+            } else {
+                return Err(syn::Error::new_spanned(kv, "Unknown attribute"));
+            }
+        }
+
+        Ok(ret)
+    }
+}
 
 /// This attribute macro generates the boilerplate required to call into the
 /// contract-specific logic from the entry-points to the Wasm module.
@@ -50,9 +89,10 @@ use syn::parse_macro_input;
 /// where `InstantiateMsg`, `ExecuteMsg`, and `QueryMsg` are contract defined
 /// types that implement `DeserializeOwned + JsonSchema`.
 #[proc_macro_attribute]
-pub fn entry_point(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
+pub fn entry_point(attr: TokenStream, mut item: TokenStream) -> TokenStream {
     let cloned = item.clone();
     let function = parse_macro_input!(cloned as syn::ItemFn);
+    let Options { crate_path } = parse_macro_input!(attr as Options);
 
     // The first argument is `deps`, the rest is region pointers
     let args = function.sig.inputs.len() - 1;
@@ -68,7 +108,7 @@ pub fn entry_point(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
         mod #wasm_export { // new module to avoid conflict of function name
             #[no_mangle]
             extern "C" fn #fn_name(#( #decl_args : u32 ),*) -> u32 {
-                cosmwasm_std::#do_call(&super::#fn_name, #( #call_args ),*)
+                #crate_path::#do_call(&super::#fn_name, #( #call_args ),*)
             }
         }
     };
