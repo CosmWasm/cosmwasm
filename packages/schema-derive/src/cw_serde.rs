30b4627b6e998fd3c6a965d945781ba4ce72395b
Aumetra Weisman <aumetra@cryptolab.net>
2024-03-25 13:14:31 +0100

    Make `cw_serde` configurable

--- packages/schema-derive/src/cw_serde.rs
+++ packages/schema-derive/src/cw_serde.rs
@@ -1,20 +1,59 @@
 use crate::error::bail;
 use quote::{quote, ToTokens};
-use syn::DeriveInput;
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_quote,
+    punctuated::Punctuated,
+    DeriveInput, MetaNameValue, Token,
+};
 
-pub fn cw_serde_impl(input: DeriveInput) -> syn::Result<DeriveInput> {
+pub struct Options {
+    crate_path: syn::Path,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            crate_path: parse_quote!(::cosmwasm_schema),
+        }
+    }
+}
+
+impl Parse for Options {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut acc = Self::default();
+        let params = Punctuated::<MetaNameValue, Token![,]>::parse_terminated(input)?;
+        for param in params {
+            if param.path.is_ident("crate") {
+                let path_as_string: syn::LitStr = syn::parse2(param.value.to_token_stream())?;
+                acc.crate_path = path_as_string.parse()?
+            } else {
+                bail!(param, "unknown option");
+            }
+        }
+
+        Ok(acc)
+    }
+}
+
+pub fn cw_serde_impl(options: Options, input: DeriveInput) -> syn::Result<DeriveInput> {
+    let crate_path = &options.crate_path;
+    let crate_path_displayable = crate_path.to_token_stream();
+    let serde_path = format!("{crate_path_displayable}::serde");
+    let schemars_path = format!("{crate_path_displayable}::schemars");
+
     let mut stream = quote! {
         #[derive(
-            ::cosmwasm_schema::serde::Serialize,
-            ::cosmwasm_schema::serde::Deserialize,
+            #crate_path::serde::Serialize,
+            #crate_path::serde::Deserialize,
             ::std::clone::Clone,
             ::std::fmt::Debug,
             ::std::cmp::PartialEq,
-            ::cosmwasm_schema::schemars::JsonSchema
+            #crate_path::schemars::JsonSchema
         )]
         #[allow(clippy::derive_partial_eq_without_eq)] // Allow users of `#[cw_serde]` to not implement Eq without clippy complaining
-        #[serde(deny_unknown_fields, crate = "::cosmwasm_schema::serde")]
-        #[schemars(crate = "::cosmwasm_schema::schemars")]
+        #[serde(deny_unknown_fields, crate = #serde_path)]
+        #[schemars(crate = #schemars_path)]
     };
 
     match input.data {
@@ -36,12 +75,15 @@ mod tests {
 
     #[test]
     fn structs() {
-        let expanded = cw_serde_impl(parse_quote! {
-            pub struct InstantiateMsg {
-                pub verifier: String,
-                pub beneficiary: String,
-            }
-        })
+        let expanded = cw_serde_impl(
+            Options::default(),
+            parse_quote! {
+                pub struct InstantiateMsg {
+                    pub verifier: String,
+                    pub beneficiary: String,
+                }
+            },
+        )
         .unwrap();
 
         let expected = parse_quote! {
@@ -67,9 +109,12 @@ mod tests {
 
     #[test]
     fn empty_struct() {
-        let expanded = cw_serde_impl(parse_quote! {
-            pub struct InstantiateMsg {}
-        })
+        let expanded = cw_serde_impl(
+            Options::default(),
+            parse_quote! {
+                pub struct InstantiateMsg {}
+            },
+        )
         .unwrap();
 
         let expected = parse_quote! {
@@ -92,14 +137,17 @@ mod tests {
 
     #[test]
     fn enums() {
-        let expanded = cw_serde_impl(parse_quote! {
-            pub enum SudoMsg {
-                StealFunds {
-                    recipient: String,
-                    amount: Vec<Coin>,
-                },
-            }
-        })
+        let expanded = cw_serde_impl(
+            Options::default(),
+            parse_quote! {
+                pub enum SudoMsg {
+                    StealFunds {
+                        recipient: String,
+                        amount: Vec<Coin>,
+                    },
+                }
+            },
+        )
         .unwrap();
 
         let expected = parse_quote! {
@@ -129,12 +177,15 @@ mod tests {
     #[test]
     #[should_panic(expected = "unions are not supported")]
     fn unions() {
-        cw_serde_impl(parse_quote! {
-            pub union SudoMsg {
-                x: u32,
-                y: u32,
-            }
-        })
+        cw_serde_impl(
+            Options::default(),
+            parse_quote! {
+                pub union SudoMsg {
+                    x: u32,
+                    y: u32,
+                }
+            },
+        )
         .unwrap();
     }
 }
