a915beb70bbaba9b043a6592eac3129d2d10fbd7
Aumetra Weisman <aumetra@cryptolab.net>
2024-04-10 12:59:04 +0200

    Add BLS verify tests

--- packages/crypto/tests/bls12_381.rs
+++ packages/crypto/tests/bls12_381.rs
@@ -1,8 +1,14 @@
 #![cfg(feature = "std")]
 
+use std::{error::Error, fs};
+
 use base64::engine::general_purpose::STANDARD;
 use base64_serde::base64_serde_type;
-use cosmwasm_crypto::{bls12_381_aggregate_g1, bls12_381_aggregate_g2};
+use cosmwasm_crypto::{
+    bls12_381_aggregate_g1, bls12_381_aggregate_g2, bls12_381_g1_generator,
+    bls12_381_g1_is_identity, bls12_381_g2_is_identity, bls12_381_hash_to_g2,
+    bls12_381_pairing_equality, HashFunction,
+};
 
 base64_serde_type!(Base64Standard, STANDARD);
 
@@ -29,6 +35,19 @@ struct AggregateTestFile {
 struct AggregateTest {
     input: Vec<Vec<u8>>,
     output: Option<Vec<u8>>,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct VerifyTestInput {
+    pubkey: String,
+    message: String,
+    signature: String,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct VerifyTestFile {
+    input: VerifyTestInput,
+    output: bool,
 }
 
 const ETH_HEADER_FILE: &str = include_str!("../testdata/eth-headers/1699693797.394876721s.json");
@@ -60,7 +79,7 @@ fn read_aggregate_test(json: &str) -> AggregateTest {
 
 // Test for https://eth2book.info/capella/part2/building_blocks/signatures/#aggregating-public-keys
 #[test]
-fn bls12_318_aggregate_g1_works() {
+fn bls12_381_aggregate_g1_works() {
     let file = read_eth_header_file();
 
     let pubkeys: Vec<&[u8]> = file.public_keys.iter().map(|m| m.0.as_slice()).collect();
@@ -72,7 +91,7 @@ fn bls12_318_aggregate_g1_works() {
 
 // Test for https://eth2book.info/capella/part2/building_blocks/signatures/#aggregating-signatures
 #[test]
-fn bls12_318_aggregate_g2_works() {
+fn bls12_381_aggregate_g2_works() {
     for json in [
         AGGREGATE_1,
         AGGREGATE_2,
@@ -101,5 +120,69 @@ fn bls12_318_aggregate_g2_works() {
                 ]
             ),
         }
+    }
+}
+
+#[test]
+fn bls12_381_verify_works() {
+    let paths = glob::glob("testdata/bls-tests/verify/*.json")
+        .unwrap()
+        .flatten();
+
+    for path in paths {
+        let test_data = fs::read(&path).unwrap();
+        let test_data: VerifyTestFile = serde_json::from_slice(&test_data).unwrap();
+
+        let pubkey = hex::decode(&test_data.input.pubkey[2..]).unwrap();
+        let message = hex::decode(&test_data.input.message[2..]).unwrap();
+        let signature = hex::decode(&test_data.input.signature[2..]).unwrap();
+
+        let message_point = bls12_381_hash_to_g2(
+            HashFunction::Sha256,
+            &message,
+            b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_",
+        );
+
+        let pubkey = pubkey.try_into().unwrap();
+        let signature = signature.try_into().unwrap();
+
+        let verify_result = (|| {
+            if bls12_381_g1_is_identity(&pubkey)? {
+                println!("pubkey is identity");
+                return Ok(false);
+            }
+
+            if bls12_381_g2_is_identity(&signature)? {
+                println!("signature is identity");
+                return Ok(false);
+            }
+
+            let bool_result = bls12_381_pairing_equality(
+                &pubkey,
+                &message_point,
+                &bls12_381_g1_generator(),
+                &signature,
+            )?;
+
+            if !bool_result {
+                println!("pairing is not equal");
+            }
+
+            Ok::<_, Box<dyn Error>>(bool_result)
+        })();
+
+        let verify_result = verify_result
+            .inspect_err(|err| eprintln!("error: {err}"))
+            .unwrap_or(false);
+
+        assert_eq!(
+            verify_result,
+            test_data.output,
+            "Failed with test vector {}",
+            path.display()
+        );
+
+        println!("Finished case {}", path.display());
+        println!("========================");
     }
 }
