36b749c6b7f5fb615d05ccae23815d67c576b90c
Aumetra Weisman <aumetra@cryptolab.net>
2024-04-16 13:49:18 +0200

    Readjust gas values

--- packages/vm/src/environment.rs
+++ packages/vm/src/environment.rs
@@ -72,8 +72,8 @@ impl Default for GasConfig {
             // From https://docs.rs/ed25519-zebra/2.2.0/ed25519_zebra/batch/index.html
             ed25519_batch_verify_cost: 63 * GAS_PER_US / 2,
             ed25519_batch_verify_one_pubkey_cost: 63 * GAS_PER_US / 4,
-            bls12_381_hash_to_g1_cost: 337 * GAS_PER_US,
-            bls12_381_hash_to_g2_cost: 543 * GAS_PER_US,
+            bls12_381_hash_to_g1_cost: 324 * GAS_PER_US,
+            bls12_381_hash_to_g2_cost: 528 * GAS_PER_US,
             // god i wish i was lying
             bls12_381_pairing_equality_cost: 1318 * GAS_PER_US,
         }
