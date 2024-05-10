87808ec8a790af12737d979f2266a8d846b39aa2
Christoph Otter <chris@confio.gmbh>
2024-05-10 14:19:42 +0200

    Fix unused import warnings

--- packages/vm/src/modules/mod.rs
+++ packages/vm/src/modules/mod.rs
@@ -4,8 +4,8 @@ mod in_memory_cache;
 mod pinned_memory_cache;
 mod versioning;
 
-pub use cached_module::{engine_size_estimate, CachedModule};
-pub use file_system_cache::{FileSystemCache, NewFileSystemCacheError};
+pub use cached_module::CachedModule;
+pub use file_system_cache::FileSystemCache;
 pub use in_memory_cache::InMemoryCache;
 pub use pinned_memory_cache::PinnedMemoryCache;
 pub use versioning::current_wasmer_module_version;
