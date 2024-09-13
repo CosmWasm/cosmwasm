017a9abfa3e6339541ec50e4095ad0cabbcd9043
Aumetra Weisman <aumetra@cryptolab.net>
2024-09-13 13:55:51 +0200

    Format code using rustfmt before hashing (please don't yell at me for this D:)

--- packages/vm-derive-impl/src/hash_function.rs
+++ packages/vm-derive-impl/src/hash_function.rs
@@ -1,7 +1,32 @@
+use std::{
+    io::Write,
+    process::{Command, Stdio},
+};
+
 use proc_macro2::TokenStream;
 use quote::quote;
 
 use super::{bail, maybe};
+
+// i do what i must because i can
+fn format_code<C>(code: C) -> String
+where
+    C: AsRef<[u8]>,
+{
+    let mut child = Command::new("rustfmt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    {
+        let mut stdin = child.stdin.take().unwrap();
+        stdin.write_all(code.as_ref()).unwrap();
+    }
+
+    let output = child.wait_with_output().unwrap();
+    String::from_utf8(output.stdout).unwrap()
+}
 
 pub fn hash_function_impl(attr: TokenStream, input: TokenStream) -> TokenStream {
     if !attr.is_empty() {
@@ -11,8 +36,7 @@ pub fn hash_function_impl(attr: TokenStream, input: TokenStream) -> TokenStream 
     // Just verify that this is actually a function
     let _: syn::ItemFn = maybe!(syn::parse2(input.clone()));
 
-    let display = input.to_string();
-    panic!("display: {:?}", display);
+    let display = format_code(input.to_string());
     let hex_hash = blake3::hash(display.as_bytes()).to_hex();
     let hex_hash = hex_hash.as_str();
 
