31f136b09fc5b59365cd3767596852b71d55b692
Aumetra Weisman <aumetra@cryptolab.net>
2024-09-24 16:23:55 +0200

    Fix cherrypick issues

--- packages/derive/src/lib.rs
+++ packages/derive/src/lib.rs
@@ -4,57 +4,7 @@ extern crate syn;
 use proc_macro::TokenStream;
 use std::str::FromStr;
 
-<<<<<<< HEAD
-/// This attribute macro generates the boilerplate required to call into the
-/// contract-specific logic from the entry-points to the Wasm module.
-///
-/// It should be added to the contract's init, handle, migrate and query implementations
-/// like this:
-/// ```
-/// # use cosmwasm_std::{
-/// #     Storage, Api, Querier, DepsMut, Deps, entry_point, Env, StdError, MessageInfo,
-/// #     Response, QueryResponse,
-/// # };
-/// #
-/// # type InstantiateMsg = ();
-/// # type ExecuteMsg = ();
-/// # type QueryMsg = ();
-///
-/// #[entry_point]
-/// pub fn instantiate(
-///     deps: DepsMut,
-///     env: Env,
-///     info: MessageInfo,
-///     msg: InstantiateMsg,
-/// ) -> Result<Response, StdError> {
-/// #   Ok(Default::default())
-/// }
-///
-/// #[entry_point]
-/// pub fn execute(
-///     deps: DepsMut,
-///     env: Env,
-///     info: MessageInfo,
-///     msg: ExecuteMsg,
-/// ) -> Result<Response, StdError> {
-/// #   Ok(Default::default())
-/// }
-///
-/// #[entry_point]
-/// pub fn query(
-///     deps: Deps,
-///     env: Env,
-///     msg: QueryMsg,
-/// ) -> Result<QueryResponse, StdError> {
-/// #   Ok(Default::default())
-/// }
-/// ```
-///
-/// where `InstantiateMsg`, `ExecuteMsg`, and `QueryMsg` are contract defined
-/// types that implement `DeserializeOwned + JsonSchema`.
-=======
 // function documented in cosmwasm-std
->>>>>>> e230048f1 (Document entry_point in cosmwasm-std instead of derive)
 #[proc_macro_attribute]
 pub fn entry_point(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
     let cloned = item.clone();
