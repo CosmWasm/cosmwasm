12f8dfe09dd3ce45a4d6a7a1a0a883222b7bd93a
Christoph Otter <chris@confio.gmbh>
2024-10-07 17:25:15 +0700

    Suppress linter warning
    (cherry picked from commit ca69baf93696ff2b8fd3a1762556e65e457cd8a6)

--- packages/std/src/math/num_consts.rs
+++ packages/std/src/math/num_consts.rs
@@ -1,4 +1,5 @@
 /// Crate internal trait for all our signed and unsigned number types
+#[allow(dead_code)] // only used in tests for now
 pub(crate) trait NumConsts {
     const MAX: Self;
     const MIN: Self;
