8538b9c32c6b37dd160f5c188f5398145951e0f2
Simon Warta <simon@warta.it>
2025-03-06 16:50:21 +0100

    Use grow_be_int to write sign extension code more compact

--- packages/std/src/math/int256.rs
+++ packages/std/src/math/int256.rs
@@ -65,20 +65,7 @@ impl Int256 {
     /// [`from_be_bytes`]: Self::from_be_bytes
     #[inline]
     pub const fn new(value: i128) -> Self {
-        // See https://en.wikipedia.org/wiki/Sign_extension
-        let b = value.to_be_bytes();
-        if value.is_negative() {
-            Self::from_be_bytes([
-                0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-                0xFF, 0xFF, b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8], b[9], b[10],
-                b[11], b[12], b[13], b[14], b[15],
-            ])
-        } else {
-            Self::from_be_bytes([
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, b[0], b[1], b[2], b[3], b[4], b[5],
-                b[6], b[7], b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15],
-            ])
-        }
+        Self::from_be_bytes(grow_be_int(value.to_be_bytes()))
     }
 
     /// Creates a Int256(0)
