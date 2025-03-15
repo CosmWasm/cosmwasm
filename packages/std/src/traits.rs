ccd6b3e82270a9ca99d70318fafb3ef8ce4c23a6
BigtoC <taitocdt@gmail.com>
2025-03-15 17:24:17 +0800

    chore(crate): Fix struct imports

--- packages/std/src/traits.rs
+++ packages/std/src/traits.rs
@@ -6,15 +6,13 @@ use serde::{de::DeserializeOwned, Serialize};
 use crate::coin::Coin;
 #[cfg(feature = "iterator")]
 use crate::iterator::{Order, Record};
-use crate::metadata::NullableDenomMetadata;
 use crate::prelude::*;
 #[cfg(feature = "cosmwasm_1_2")]
 use crate::query::CodeInfoResponse;
 #[cfg(feature = "cosmwasm_1_1")]
 use crate::query::SupplyResponse;
 use crate::query::{
-    AllBalanceResponse, BalanceResponse, BankQuery, CustomQuery, NullableDenomMetadataResponse,
-    QueryRequest, WasmQuery,
+    AllBalanceResponse, BalanceResponse, BankQuery, CustomQuery, QueryRequest, WasmQuery,
 };
 #[cfg(feature = "staking")]
 use crate::query::{
@@ -24,14 +22,14 @@ use crate::query::{
 #[cfg(feature = "cosmwasm_1_3")]
 use crate::query::{
     AllDenomMetadataResponse, DelegatorWithdrawAddressResponse, DenomMetadataResponse,
-    DistributionQuery,
+    DistributionQuery, NullableDenomMetadataResponse,
 };
 use crate::results::{ContractResult, Empty, SystemResult};
 use crate::ContractInfoResponse;
 use crate::{from_json, to_json_binary, to_json_vec, Binary};
 use crate::{Addr, CanonicalAddr};
 #[cfg(feature = "cosmwasm_1_3")]
-use crate::{DenomMetadata, PageRequest};
+use crate::{DenomMetadata, NullableDenomMetadata, PageRequest};
 use crate::{RecoverPubkeyError, StdError, StdResult, VerificationError};
 
 #[derive(Clone, Copy, Debug)]
