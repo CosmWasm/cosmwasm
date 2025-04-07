baa10266522332c5766c5c5a0f9acb84bfe5500a
Bigto Chan <taitocdt@gmail.com>
2025-04-08 00:34:14 +0800

    Delete redundant test
    Co-authored-by: Christoph Otter <chipshort@tutanota.com>

--- packages/std/src/metadata.rs
+++ packages/std/src/metadata.rs
@@ -108,9 +108,8 @@ mod unit_tests {
         assert_eq!(metadata_with_null_aliases.denom_units.len(), 1);
         assert_eq!(
             metadata_with_null_aliases.denom_units[0].aliases,
-            Vec::<String>::default()
+            Vec::<String>::new()
         );
-        assert!(metadata_with_null_aliases.denom_units[0].aliases.is_empty());
     }
 
     #[test]
