6d635bf216cbb6545717cfcc687d1ab4f9dd9b9e
Tomasz Kulik <tomek.kulik2@gmail.com>
2025-04-11 11:16:49 +0200

    feat: Add WriteAcknowledgement to Ibc2Msg (#2425)

--- contracts/ibc2/src/msg.rs
+++ contracts/ibc2/src/msg.rs
@@ -6,3 +6,9 @@ pub enum QueryMsg {
     #[returns(crate::state::State)]
     QueryState {},
 }
+
+#[cw_serde]
+pub struct IbcPayload {
+    pub response_without_ack: bool,
+    pub send_async_ack_for_prev_msg: bool,
+}
