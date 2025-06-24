0772d6c94a63360f685d641ac6f7dc630bca62ef
Simon Warta <simon@warta.it>
2025-06-24 11:01:46 +0200

    Update schema generation scripts

--- contracts/ibc-reflect/src/bin/schema.rs
+++ contracts/ibc-reflect/src/bin/schema.rs
@@ -1,4 +1,5 @@
 use std::env::current_dir;
+use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, export_schema_with_title, schema_for, write_api};
 use cosmwasm_std::Empty;
@@ -21,6 +22,7 @@ fn main() {
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema");
     out_dir.push("ibc");
+    create_dir_all(&out_dir).unwrap();
     export_schema(&schema_for!(PacketMsg), &out_dir);
     export_schema_with_title(
         &schema_for!(AcknowledgementMsg<BalanceResponse>),
