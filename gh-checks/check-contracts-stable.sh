d9785b98543e8ea3dfce0ce664f5b5b70e8f8a49
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-08-07 12:23:43 +0200

    Updates.

--- gh-checks/check-contracts-stable.sh
+++ gh-checks/check-contracts-stable.sh
@@ -4,7 +4,11 @@ set -o errexit -o nounset -o pipefail
 
 for dir in contracts/*/; do
 (
+  echo "Change working directory to: $dir"
   cd "$dir"
-  pwd
+
+  echo "Check formatting"
+  cargo fmt -- --check
+
 )
 done
