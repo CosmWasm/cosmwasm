dcc89f62f2030ea46b4342656d3e0ddce768479f
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-08-11 13:00:23 +0200

    Updates.

--- task-actions/check-contracts.sh
+++ task-actions/check-contracts.sh
@@ -2,7 +2,95 @@
 
 set -o errexit -o nounset -o pipefail
 
-source ./task-actions/check-contract.sh
+if [[ "${1:-}" == "parallel" ]]; then
+  parallel=1
+else
+  parallel=0
+fi
+
+msg() {
+  if (( !parallel )); then
+    echo -e "\e[1;34m$1\e[0m \e[1;32m$2\e[0m"
+  fi
+}
+
+check_contract_stable() {
+  toolchain=1.82.0
+  (
+    contract_dir=$1
+    contract="$(basename "$contract_dir" | tr - _)"
+    wasm="./target/wasm32-unknown-unknown/release/$contract.wasm"
+
+    msg "CHANGE DIRECTORY" "$contract_dir"
+    cd "$contract_dir" || exit 1
+
+    msg "CHECK FORMATTING" "$contract"
+    cargo +$toolchain fmt -- --check
+
+    msg "RUN UNIT TESTS" "$contract"
+    cargo +$toolchain test --lib --locked
+
+    msg "BUILD WASM" "$contract"
+    cargo +$toolchain build --release --lib --locked --target wasm32-unknown-unknown
+
+    msg "RUN LINTER" "$contract"
+    cargo +$toolchain clippy --all-targets --tests -- -D warnings
+
+    msg "RUN INTEGRATION TESTS" "$contract"
+    cargo +$toolchain test --test integration --locked
+
+    msg "GENERATE SCHEMA" "$contract"
+    cargo +$toolchain run --bin schema --locked
+
+    msg "ENSURE SCHEMA IS UP-TO-DATE" "$contract"
+    git diff --quiet ./schema
+
+    msg "cosmwasm-check (release)" "$contract"
+    cosmwasm-check-release "$wasm"
+
+    msg "cosmwasm-check (develop)" "$contract"
+    cosmwasm-check "$wasm"
+  )
+}
+
+check_contract_nightly() {
+  toolchain=nightly-2024-09-01
+  (
+    contract_dir=$1
+    contract="$(basename "$contract_dir" | tr - _)"
+    wasm="./target/wasm32-unknown-unknown/release/$contract.wasm"
+
+    msg "CHANGE DIRECTORY" "$contract_dir"
+    cd "$contract_dir" || exit 1
+
+    msg "CHECK FORMATTING" "$contract"
+    cargo +$toolchain fmt -- --check
+
+    msg "RUN UNIT TESTS" "$contract"
+    cargo +$toolchain test --lib --locked
+
+    msg "BUILD WASM" "$contract"
+    RUSTFLAGS="-C target-feature=+nontrapping-fptoint" cargo +$toolchain build --release --lib --locked --target wasm32-unknown-unknown
+
+    msg "RUN LINTER" "$contract"
+    cargo +$toolchain clippy --all-targets --tests -- -D warnings
+
+    msg "RUN INTEGRATION TESTS" "$contract"
+    cargo +$toolchain test --test integration --locked
+
+    msg "GENERATE SCHEMA" "$contract"
+    cargo +$toolchain run --bin schema --locked
+
+    msg "ENSURE SCHEMA IS UP-TO-DATE" "$contract"
+    git diff --quiet ./schema
+
+    msg "cosmwasm-check (release)" "$contract"
+    cosmwasm-check-release "$wasm"
+
+    msg "cosmwasm-check (develop)" "$contract"
+    cosmwasm-check  "$wasm"
+  )
+}
 
 contracts_stable=(
   contracts/burner
@@ -26,12 +114,12 @@ contracts_nightly=(
   contracts/floaty
 )
 
-if [[ "${1:-}" == "parallel" ]]; then
+if (( parallel )); then
   for dir in "${contracts_stable[@]}"; do
-    check_contract_stable "$dir" &
+    check_contract_stable "$dir" > /dev/null &
   done
   for dir in "${contracts_nightly[@]}"; do
-    check_contract_nightly "$dir" &
+    check_contract_nightly "$dir" > /dev/null &
   done
   wait
 else
