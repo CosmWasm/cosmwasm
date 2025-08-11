044f91c63212cecaae98d90757c03223faa17294
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-08-11 11:14:13 +0200

    Updates.

--- task-actions/clean-contracts.sh
+++ task-actions/clean-contracts.sh
@@ -2,32 +2,11 @@
 
 set -o errexit -o nounset -o pipefail
 
-contracts=(
-  contracts/burner/
-  contracts/crypto-verify/
-  contracts/cyberpunk/
-  contracts/empty/
-  contracts/floaty/
-  contracts/hackatom/
-  contracts/ibc2/
-  contracts/ibc-callbacks/
-  contracts/ibc-reflect/
-  contracts/ibc-reflect-send/
-  contracts/nested-contracts/
-  contracts/queue/
-  contracts/reflect/
-  contracts/replier/
-  contracts/staking/
-  contracts/virus/
-)
-
-for dir in "${contracts[@]}"; do
+for dir in contracts/*/; do
   (
     contract="$(basename "$dir" | tr - _)"
-
     echo -e "\e[1;34mClean contract\e[0m \e[1;32m$contract\e[0m"
     cd "$dir" || exit 1
-
     cargo clean
   )
 done
