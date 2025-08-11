5514041c720ceb23f1f9a121e3d1befdc116baec
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-08-11 09:56:39 +0200

    Updates.

--- gh-actions/verify-contracts-stable.sh
+++ gh-actions/verify-contracts-stable.sh
@@ -27,22 +27,22 @@ for dir in "${contracts[@]}"; do
     cd "$dir" || exit 1
 
     echo -e "\e[1;34mCHECK FORMATTING\e[0m"
-    cargo fmt -- --check
+    cargo +1.82.0 fmt -- --check
 
     echo -e "\e[1;34mRUN UNIT TESTS\e[0m"
-    cargo test --lib --locked
+    cargo +1.82.0 test --lib --locked
 
     echo  -e "\e[1;34mBUILD WASM\e[0m"
-    cargo build --release --lib --locked --target wasm32-unknown-unknown
+    cargo +1.82.0 build --release --lib --locked --target wasm32-unknown-unknown
 
     echo -e "\e[1;34mRUN LINTER\e[0m"
-    cargo clippy --all-targets --tests -- -D warnings
+    cargo +1.82.0 clippy --all-targets --tests -- -D warnings
 
     echo -e "\e[1;34mRUN INTEGRATION TESTS\e[0m"
-    cargo test --test integration --locked
+    cargo +1.82.0 test --test integration --locked
 
     echo -e "\e[1;34mGENERATE SCHEMA\e[0m"
-    cargo run --bin schema --locked
+    cargo +1.82.0 run --bin schema --locked
 
     echo -e "\e[1;34mENSURE SCHEMA IS UP-TO-DATE\e[0m"
     git diff --quiet ./schema
