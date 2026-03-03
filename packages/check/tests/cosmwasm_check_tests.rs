1240508582e7ffaf495fc3053074ef63b159a62a
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-03-03 09:57:12 +0100

    Refactored tests for cosmwasm-check.

--- packages/check/tests/cosmwasm_check_tests.rs
+++ packages/check/tests/cosmwasm_check_tests.rs
@@ -1,137 +1,114 @@
-use assert_cmd::{cargo_bin, Command};
+use assert_cmd::{cargo_bin, cmd::Command};
 use cosmwasm_std::{to_json_string, to_json_vec};
 use cosmwasm_vm::WasmLimits;
-use predicates::prelude::*;
+use predicates::{boolean::PredicateBooleanExt, str::contains};
 use std::io::Write;
 use tempfile::NamedTempFile;
 
 #[test]
-fn valid_contract_check() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
-    cmd.arg("../vm/testdata/hackatom.wasm");
-    cmd.assert()
+fn valid_contract_check() {
+    Command::new(cargo_bin!())
+        .arg("../vm/testdata/hackatom.wasm")
+        .assert()
         .success()
-        .stdout(predicate::str::contains("pass"));
-
-    Ok(())
+        .stdout(contains("pass"));
 }
 
 #[test]
-fn contract_check_verbose() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
-    cmd.arg("../vm/testdata/empty.wasm").arg("--verbose");
-    cmd.assert()
+fn contract_check_verbose() {
+    Command::new(cargo_bin!())
+        .arg("../vm/testdata/empty.wasm")
+        .arg("--verbose")
+        .assert()
         .success()
-        .stdout(predicate::str::contains("pass"))
-        .stderr(predicate::str::contains("Max function parameters"));
-
-    Ok(())
+        .stdout(contains("pass"))
+        .stderr(contains("Max function parameters"));
 }
 
 #[test]
-fn empty_contract_check() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
-    cmd.arg("../vm/testdata/empty.wasm");
-    cmd.assert()
+fn empty_contract_check() {
+    Command::new(cargo_bin!())
+        .arg("../vm/testdata/empty.wasm")
+        .assert()
         .success()
-        .stdout(predicate::str::contains("pass"));
-
-    Ok(())
+        .stdout(contains("pass"));
 }
 
 #[test]
-fn invalid_contract_check() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
-    cmd.arg("../vm/testdata/corrupted.wasm");
-    cmd.assert()
+fn invalid_contract_check() {
+    Command::new(cargo_bin!())
+        .arg("../vm/testdata/corrupted.wasm")
+        .assert()
         .failure()
-        .stdout(predicate::str::contains("missing a required marker export"));
-
-    Ok(())
+        .stdout(contains("missing a required marker export"));
 }
 
 #[test]
-fn valid_contract_check_float_operator() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
-    cmd.arg("../vm/testdata/floaty.wasm");
-    cmd.assert()
+fn valid_contract_check_float_operator() {
+    Command::new(cargo_bin!())
+        .arg("../vm/testdata/floaty.wasm")
+        .assert()
         .success()
-        .stdout(predicate::str::contains("pass"));
-
-    Ok(())
+        .stdout(contains("pass"));
 }
 
 #[test]
-fn several_contracts_check() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
-    cmd.arg("../vm/testdata/hackatom.wasm")
-        .arg("../vm/testdata/corrupted.wasm");
-    cmd.assert()
+fn several_contracts_check() {
+    Command::new(cargo_bin!())
+        .arg("../vm/testdata/hackatom.wasm")
+        .arg("../vm/testdata/corrupted.wasm")
+        .assert()
         .failure()
-        .stdout(predicate::str::contains("failure"))
-        .stdout(predicate::str::contains("missing a required marker export"))
-        .stdout(predicate::str::contains("Passes: 1, failures: 1"));
-
-    Ok(())
+        .stdout(
+            contains("failure")
+                .and(contains("missing a required marker export"))
+                .and(contains("Passes: 1, failures: 1")),
+        );
 }
 
 #[test]
-fn custom_capabilities_check() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
-    cmd.arg("--available-capabilities")
+fn custom_capabilities_check() {
+    Command::new(cargo_bin!())
+    .arg("--available-capabilities")
         .arg("iterator,osmosis,friendship,cosmwasm_1_1,cosmwasm_1_2,cosmwasm_1_3,cosmwasm_1_4,cosmwasm_2_0,cosmwasm_2_1,cosmwasm_2_2")
-        .arg("../vm/testdata/hackatom.wasm");
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains("Available capabilities:"))
-        .stdout(predicate::str::contains("iterator"))
-        .stdout(predicate::str::contains("osmosis"))
-        .stdout(predicate::str::contains("friendship"));
-
-    Ok(())
+        .arg("../vm/testdata/hackatom.wasm")
+    .assert().success().stdout(
+        contains("Available capabilities:")
+            .and(contains("iterator"))
+            .and(contains("osmosis"))
+            .and(contains("friendship")),
+    );
 }
 
 #[test]
-fn wasm_limits_string_check() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
+fn wasm_limits_string_check() {
     let mut limits = WasmLimits::default();
     limits.initial_memory_limit_pages = Some(10);
+    let limits = to_json_string(&limits).unwrap();
 
-    cmd.arg("--wasm-limits")
-        .arg(to_json_string(&limits).unwrap())
-        .arg("../vm/testdata/hackatom.wasm");
-    cmd.assert()
+    Command::new(cargo_bin!())
+        .arg("--wasm-limits")
+        .arg(limits)
+        .arg("../vm/testdata/hackatom.wasm")
+        .assert()
         .failure()
-        .stdout(predicate::str::contains("must not exceed 10 pages"));
-
-    Ok(())
+        .stdout(contains("must not exceed 10 pages"));
 }
 
 #[test]
-fn wasm_limits_file_check() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::new(cargo_bin!());
-
+fn wasm_limits_file_check() {
     let mut limits = WasmLimits::default();
     limits.max_functions = Some(15);
-    let limits = to_json_vec(&limits)?;
+    let limits = to_json_vec(&limits).unwrap();
 
-    let mut tmp_file = NamedTempFile::new()?;
-    tmp_file.write_all(&limits)?;
+    let mut tmp_file = NamedTempFile::new().unwrap();
+    tmp_file.write_all(&limits).unwrap();
 
-    cmd.arg("--wasm-limits")
+    Command::new(cargo_bin!())
+        .arg("--wasm-limits")
         .arg(tmp_file.path())
-        .arg("../vm/testdata/hackatom.wasm");
-    cmd.assert()
+        .arg("../vm/testdata/hackatom.wasm")
+        .assert()
         .failure()
-        .stdout(predicate::str::contains("more than 15 functions"));
-
-    Ok(())
+        .stdout(contains("more than 15 functions"));
 }
